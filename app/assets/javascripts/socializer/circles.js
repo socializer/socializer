c73b7a943a45e73b2e05094aed01319f5dcc2ddf
Dominic Goulet <dominic.goulet@gmail.com>
2011-12-09 12:00:54 -0500

    Fixed some issues about circles and groups

--- app/assets/javascripts/socializer/circles.js
+++ app/assets/javascripts/socializer/circles.js
@@ -4,17 +4,27 @@ $(document).ready(function() {
 	$(".draggable").draggable({ revert: true });
 	$(".droppable").droppable({
 		drop: function( event, ui ) {
-			var circle_id = $("a", this).attr("data-object-id");
-			var person_id = ui.draggable.attr("data-object-id");
-			var circle_name = $("a", this).text();
-			var person_name = ui.draggable.attr("title");
+			
+			var circle = $("a", this);
+			var person = ui.draggable;
+			var circle_id = circle.attr("data-object-id");
+			var person_id = person.attr("data-object-id");
+			var circle_name = circle.text();
+			var person_name = $("img", person).attr("oldtitle");
+			var circle_tie_count = $(".circle-tie-count", this)
+			
       		$.post( "/ties", { "tie[circle_id]" : circle_id, "tie[contact_id]" : person_id } ).success(function() {
+	
+			  var tie_count = parseInt(circle_tie_count.html()) + 1;
+	          circle_tie_count.html(tie_count.toString())
+	
 			  var text = person_name + " was added to your " + circle_name + " circle.";
 			  $(".circle-info-message").html(text);
 			  $(".circle-info-message").show();
 			  setTimeout(function() {
 				$(".circle-info-message").hide();
 			  }, 5000)
+			
 		    });
 		}
 	});
