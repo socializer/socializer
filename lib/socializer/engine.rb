e1d2dfd7d85778548888ee4153c11b2f7c1170d9
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-04 16:16:55 -0400

    add initial generator configuration

--- lib/socializer/engine.rb
+++ lib/socializer/engine.rb
@@ -1,5 +1,10 @@
 module Socializer
   class Engine < Rails::Engine
     isolate_namespace Socializer
+
+    config.generators do |g|
+      g.test_framework      :rspec,        :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
