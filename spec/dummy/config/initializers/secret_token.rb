91f7ccc732451bd9f66ce7bea99c4e16a16b6674
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-05 17:19:26 -0400

    run combust and remove the dummy app

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '8359d31805f0ffb9df242505083c13ab26b5631ebe04f85d66f63d3ad871b3aa137e6f088e883c8e1d9b763d55ff9fc322319809e2aedb8d20a405ee5bfe3b1b'
