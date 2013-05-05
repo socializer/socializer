4418baa834bb0ad29a0ff01ede47ea29613671d1
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-05 13:00:13 -0400

    add database_cleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,4 +45,18 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    # DatabaseCleaner.strategy = :transaction
+    # DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
