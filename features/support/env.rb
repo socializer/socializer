50885efba08439c2bf14ca43ad8e6e97cf85d896
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-05 17:32:43 -0400

    move non-default settings to separate files and reset env.rb back to the default

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,12 +5,6 @@
 # files.
 
 require 'cucumber/rails'
-require 'cucumber/rails/rspec'
-require 'cucumber/rails/world'
-
-require 'capybara'
-require 'capybara/rails'
-# require 'capybara/cucumber'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
@@ -61,6 +55,4 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
-World(FactoryGirl::Syntax::Methods)
 
