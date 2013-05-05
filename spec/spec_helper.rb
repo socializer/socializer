f3b5e2f7012977c6858b037eb2facb70fc6bc7c1
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-05 17:13:33 -0400

    add the combustion gem
    https://github.com/pat/combustion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+# require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+
+require 'combustion'
+
+Combustion.initialize!
 
 require 'rspec/rails'
 require 'rspec/autorun'
