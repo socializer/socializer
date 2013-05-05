b0eaa94ccfc5dda76c6b04fc232930edfb77f5d2
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-05 12:56:49 -0400

    add requires to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,18 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+
 require 'rspec/rails'
 require 'rspec/autorun'
+
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'capybara/cucumber'
+
+require 'factory_girl'
+require 'database_cleaner'
+require 'shoulda-matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
