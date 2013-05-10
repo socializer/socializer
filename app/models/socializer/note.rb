44ba99628a9a27ffc83662829db55335a59db017
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-10 15:52:11 -0400

    add a fixme. notes appear to be saving twice.

--- app/models/socializer/note.rb
+++ app/models/socializer/note.rb
@@ -1,14 +1,15 @@
+# FIXME: Notes appear to be saving twice
 module Socializer
   class Note < ActiveRecord::Base
     include Socializer::Object
-    
+
     attr_accessible :content
-    
+
     belongs_to :embedded_author, :class_name => 'EmbeddedObject', :foreign_key => 'author_id'
-    
+
     def author
       @author ||= embedded_author.embeddable
     end
-    
+
   end
 end
