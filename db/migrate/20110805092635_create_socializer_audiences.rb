33ace452f51cce8351b2ddc1c6b287b70fc94d1b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-17 11:02:13 -0400

    rename scope to privacy_level, change to integer, and add index.
    #37

--- db/migrate/20110805092635_create_socializer_audiences.rb
+++ db/migrate/20110805092635_create_socializer_audiences.rb
@@ -3,11 +3,12 @@ class CreateSocializerAudiences < ActiveRecord::Migration
     create_table :socializer_audiences, :id => false do |t|
       t.integer  :activity_id
       t.integer  :object_id
-      t.string   :scope
+      t.integer  :privacy_level
 
       t.timestamps
     end
-    
+
     add_index :socializer_audiences, [ :activity_id, :object_id ], :unique => true
+    add_index :socializer_audiences, :privacy_level
   end
 end
