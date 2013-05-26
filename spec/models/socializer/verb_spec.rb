2ef7f60a986792dc50087aae6d12131ef058e2a4
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-26 16:37:17 -0400

    create a model spec for verb

--- spec/models/socializer/verb_spec.rb
+++ spec/models/socializer/verb_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 module Socializer
   describe Verb do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it { should allow_mass_assignment_of(:name) }
+    # it { should have_many(:activities) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_presence_of(:name) }
+
+    it "has a valid factory" do
+      expect(create(:socializer_verb)).to be_valid
+    end
+
+    it "is valid without a name" do
+      expect(build(:socializer_verb, name: 'post')).to be_valid
+    end
+
+    it "is invalid without a name" do
+      expect(build(:socializer_verb, name: nil)).to have(1).errors_on(:name)
+    end
+
+    it "is invalid with a duplicate name" do
+      expect(create(:socializer_verb, name: 'post')).to be_valid
+      expect(build(:socializer_verb, name: 'post')).to have(1).errors_on(:name)
+    end
   end
 end
