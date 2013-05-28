f7eefc5f4a42919bbab377d7dd8624d2a8e6441c
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-05-28 11:06:12 -0400

    add some initial model test for ActivityField

--- spec/models/socializer/activity_field_spec.rb
+++ spec/models/socializer/activity_field_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 module Socializer
   describe ActivityField do
-    pending "add some examples to (or delete) #{__FILE__}"
+    pending "add more examples to #{__FILE__}"
+
+    it { should allow_mass_assignment_of(:content) }
+    it { should allow_mass_assignment_of(:activity) }
+    it { should belong_to(:activity) }
+    it { should validate_presence_of(:content) }
+    it { should validate_presence_of(:activity) }
   end
 end
