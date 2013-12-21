e16dfd3fea633425491603bf6052b4a9b0d75733
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2013-12-21 15:02:38 -0500

    Align the parameters of a method call if they span more than one line.

--- lib/socializer/engine.rb
+++ lib/socializer/engine.rb
@@ -8,12 +8,12 @@ module Socializer
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: true,
-        controller_specs: true,
-        request_specs: false
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: true,
+                       controller_specs: true,
+                       request_specs: false
       g.integration_tool    false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
