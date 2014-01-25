c157804e7702c8b903911b60cc6a676c3a7c2496
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-01-24 20:19:39 -0500

    prefer eq over ==

--- spec/inflector_spec.rb
+++ spec/inflector_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe ActiveSupport::Inflector::Inflections do
   context 'singularize' do
     it 'should  return the singular form of a word' do
-      'ties'.singularize(:en).should == 'tie'
-      'Ties'.singularize(:en).should == 'Tie'
-      'activities'.singularize(:en).should == 'activity'
-      'Activities'.singularize(:en).should == 'Activity'
+      'ties'.singularize(:en).should eq('tie')
+      'Ties'.singularize(:en).should eq('Tie')
+      'activities'.singularize(:en).should eq('activity')
+      'Activities'.singularize(:en).should eq('Activity')
     end
 
     it 'should not alter an already singular word' do
-      'tie'.singularize(:en).should == 'tie'
-      'Tie'.singularize(:en).should == 'Tie'
-      'activity'.singularize(:en).should == 'activity'
-      'Activity'.singularize(:en).should == 'Activity'
+      'tie'.singularize(:en).should eq('tie')
+      'Tie'.singularize(:en).should eq('Tie')
+      'activity'.singularize(:en).should eq('activity')
+      'Activity'.singularize(:en).should eq('Activity')
     end
   end
 end
