e974124490f3fbbf3238b5be5fc8fe356d71d23f
Dominic Goulet <dominic.goulet@gmail.com>
2014-01-25 15:31:36 -0500

    Added current user language if signed in and no locale param is present

--- app/controllers/socializer/application_controller.rb
+++ app/controllers/socializer/application_controller.rb
@@ -21,7 +21,11 @@ module Socializer
     end
 
     def set_locale
-      I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
+      if signed_in? && current_user.language.present? && params[:locale].nil?
+        I18n.locale =  current_user.language
+      else
+        I18n.locale =  params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
+      end
     end
 
     def extract_locale_from_accept_language_header
