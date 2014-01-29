f1094d80b0c1a3e7c24cfab1e007e01b5a6159c0
Dominic Goulet <dominic.goulet@gmail.com>
2014-01-29 13:24:14 -0500

    Basic Tie spec.

--- spec/models/socializer/tie_spec.rb
+++ spec/models/socializer/tie_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 module Socializer
   describe Tie do
+    let(:tie) { build(:socializer_tie) }
+
     it 'has a valid factory' do
-      expect(build(:socializer_tie)).to be_valid
+      expect(tie).to be_valid
+    end
+
+    it '#circle' do
+      expect(tie).to respond_to(:circle)
+    end
+
+    it '#contact' do
+      expect(tie).to respond_to(:contact)
     end
   end
 end
