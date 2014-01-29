be12be0453580fead457d5149402934e38dad8f3
Martin Goulet <martin.goulet@live.com>
2014-01-29 17:12:45 -0500

    Correction factories for rspec 'valid factory'

--- spec/factories/socializer_identities.rb
+++ spec/factories/socializer_identities.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :socializer_identity, class: Socializer::Identity do
+  factory :socializer_identity, class: Socializer::Identity do |f|
+    f.sequence(:name) { |n| "name#{n}"}
+    f.sequence(:email) { |n| "name#{n}@example.com"}
+    f.sequence(:password) { |n| "foobar"}
+    f.sequence(:password_confirmation) { |n| "foobar"}
   end
 end
