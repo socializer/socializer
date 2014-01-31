db979f80053f6574c6b0c2e845a29e8f0404ce07
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-01-31 10:26:57 -0500

    Prefer single-quoted strings when you don't need string interpolation or special symbols

--- spec/factories/socializer_identities.rb
+++ spec/factories/socializer_identities.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :socializer_identity, class: Socializer::Identity do
     sequence(:name) { |n| "name#{n}" }
     sequence(:email) { |n| "name#{n}@example.com" }
-    sequence(:password) { |n| "foobar" }
-    sequence(:password_confirmation) { |n| "foobar" }
+    sequence(:password) { |n| 'foobar' }
+    sequence(:password_confirmation) { |n| 'foobar' }
   end
 end
