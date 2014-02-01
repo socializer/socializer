5056bbd331621fe59bcd7c41d74d0f80fcb09cd4
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-01 16:53:11 -0500

    Use the new Ruby 1.9 hash syntax.

--- spec/factories/socializer_notifications.rb
+++ spec/factories/socializer_notifications.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :socializer_notification, :class => Socializer::Notification do
+  factory :socializer_notification, class: Socializer::Notification do
     displayed false
     read false
 
