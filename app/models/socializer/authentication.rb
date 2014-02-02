2d77efe062d89d98608da35ca44b5d3134197e96
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-02 17:34:46 -0500

    organization and FIXME comment

--- app/models/socializer/authentication.rb
+++ app/models/socializer/authentication.rb
@@ -1,8 +1,8 @@
 module Socializer
   class Authentication < ActiveRecord::Base
-    belongs_to :person
-
     attr_accessible :provider, :uid, :image_url
+
+    belongs_to :person
 
     before_destroy :make_sure_its_not_the_last_one
 
@@ -10,6 +10,9 @@ module Socializer
 
     def make_sure_its_not_the_last_one
       if person.authentications.count == 1
+        # FIXME: authentication - This is not be a good user experience.
+        #       If the user clicks 'unbind' on their last authentication they will get an error.
+        #       A flash notice should be set and the user should be able to continue on.
         fail 'Cannot delete the last authentication available.'
       end
     end
