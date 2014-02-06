176a805b396508f9ed15a286e80be509fea4245f
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-06 13:36:25 -0500

    add display_name and email to the person factory

--- spec/factories/socializer_people.rb
+++ spec/factories/socializer_people.rb
@@ -2,6 +2,8 @@
 
 FactoryGirl.define do
   factory :socializer_person, class: Socializer::Person do
+    sequence(:display_name) { |n| "name#{n}" }
+    sequence(:email) { |n| "name#{n}@example.com" }
     avatar_provider 'GRAVATAR'
   end
 end
