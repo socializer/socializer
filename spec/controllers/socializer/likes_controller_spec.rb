2d9cb2d5f3d59a4f64f04a41c66776ee8fc50b33
Martin Goulet <martin.goulet@live.ca>
2014-02-12 07:50:25 -0500

    Add tests for likes_controller

--- spec/controllers/socializer/likes_controller_spec.rb
+++ spec/controllers/socializer/likes_controller_spec.rb
@@ -2,28 +2,76 @@ require 'spec_helper'
 
 module Socializer
   describe LikesController do
-    pending "add additional examples to #{__FILE__}"
 
-    # describe "GET 'index'" do
-    #   it "returns http success" do
-    #     get 'index'
-    #     response.should be_success
-    #   end
-    # end
+    # before and after are necessary since shares_controller isn't accessible directly in routes.
+    before(:all) do
+      Rails.application.routes.draw { get ':controller(/:action)'  }
+    end
 
-    # describe "GET 'create'" do
-    #   it "returns http success" do
-    #     get 'create'
-    #     response.should be_success
-    #   end
-    # end
+    after(:all) do
+      Rails.application.reload_routes!
+    end
 
-    # describe "GET 'destroy'" do
-    #   it "returns http success" do
-    #     get 'destroy'
-    #     response.should be_success
-    #   end
-    # end
+    # Create a user and a activity
+    let(:user) { create(:socializer_person) }
+    let(:note_activity) { create(:socializer_activity) }
 
+    # Setting the current user
+    before { cookies[:user_id] = user.guid }
+
+    describe 'Set likable and activity' do
+      # Verify that the likable variable is set before create and destroy action
+      [:create, :destroy].each do |action|
+        before { get action, id: note_activity.activity_object.id, format: :js }
+
+        it 'should set likable for action #{action}' do
+          expect(assigns(:likable)).to eq(note_activity.activity_object)
+        end
+
+        it 'should set activity for action #{action}' do
+          expect(assigns(:activity)).to eq(note_activity)
+        end
+      end
+    end
+
+    # Make sure that the note is not liked before liking it.
+    it 'should not likes the note before liking it' do
+      expect( user.likes?(note_activity.activity_object) ).to be_false
+    end
+
+    describe 'GET #create' do
+      # Create a like
+      before { get :create, id: note_activity.activity_object.id, format: :js }
+
+      it 'should likes the note after liking it' do
+        expect( user.likes?(note_activity.activity_object) ).to be_true
+      end
+    end
+
+    describe 'GET #destroy' do
+      # Create a like
+      before { get :create,  id: note_activity.activity_object.id, format: :js }
+      # Destroy the like
+      before { get :destroy, id: note_activity.activity_object.id, format: :js }
+
+      it 'should not likes the note anymore' do
+        expect( user.likes?(note_activity.activity_object) ).to be_false
+      end
+    end
+
+    describe 'GET #index' do
+      # Create a like
+      before { get :create, id: note_activity.activity_object.id, format: :js }
+      # Get the people ou like the activity
+      before { get :index,  id: note_activity.id, format: :html }
+
+      it 'should return people' do
+        expect(assigns(:object_ids)).to be_present
+      end
+
+      it 'should return the user who like the activity' do
+        expect(assigns(:object_ids)).to include(user.activity_object)
+      end
+    end
   end
 end
