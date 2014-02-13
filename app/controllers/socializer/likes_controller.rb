f21cd16e1efa9a54599cd43baec933aea28014fa
Martin Goulet <martin.goulet@live.ca>
2014-02-13 11:28:53 -0500

    rename set_likable to set_likable_and_activity

--- app/controllers/socializer/likes_controller.rb
+++ app/controllers/socializer/likes_controller.rb
@@ -36,7 +36,7 @@ module Socializer
     private
 
     # Use callbacks to share common setup or constraints between actions.
-    def set_likable
+    def set_likable_and_activity
       @likable = ActivityObject.find(params[:id])
       @activity = @likable.activitable
     end
