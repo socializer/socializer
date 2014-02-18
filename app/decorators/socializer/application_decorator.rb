d78202a94eedf2c98ddadc5eff3f50c2e151442e
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-18 11:52:57 -0500

    convert the time_ago helper to the created_at_time_ago decorator method

--- app/decorators/socializer/application_decorator.rb
+++ app/decorators/socializer/application_decorator.rb
@@ -1,2 +1,20 @@
-class ApplicationDecorator < Draper::Decorator
+module Socializer
+  class ApplicationDecorator < Draper::Decorator
+    def created_at_time_ago(options = {})
+      time_ago(model.created_at, options)
+    end
+
+    private
+
+    def time_ago(time, options = {})
+      time = time.to_time.utc
+
+      options.reverse_merge! title: l(time, format: :short)
+
+      options[:data] ||= {}
+      options[:data].merge! time_ago: 'moment.js'
+
+      h.time_tag time, time.strftime('%B %e, %Y %l:%M%P'), options
+    end
+  end
 end
