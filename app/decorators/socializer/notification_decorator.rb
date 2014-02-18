746ad315f23727267d492e5f0d8cc5fe285fa0d2
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-18 11:53:50 -0500

    add the socializer module

--- app/decorators/socializer/notification_decorator.rb
+++ app/decorators/socializer/notification_decorator.rb
@@ -1,13 +1,14 @@
-class NotificationDecorator < ApplicationDecorator
-  delegate_all
+module Socializer
+  class NotificationDecorator < ApplicationDecorator
+    delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+    # Define presentation-specific methods here. Helpers are accessed through
+    # `helpers` (aka `h`). You can override attributes, for example:
+    #
+    #   def created_at
+    #     helpers.content_tag :span, class: 'time' do
+    #       object.created_at.strftime("%a %m/%d/%y")
+    #     end
+    #   end
+  end
 end
