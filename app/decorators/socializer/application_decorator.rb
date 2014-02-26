87987f8bcec494638f628f05ccf6e469a920ba12
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-26 12:07:05 -0500

    allow multi-line tooltip showing the created at and edited date and time

--- app/decorators/socializer/application_decorator.rb
+++ app/decorators/socializer/application_decorator.rb
@@ -1,20 +1,34 @@
 module Socializer
   class ApplicationDecorator < Draper::Decorator
     def created_at_time_ago(options = {})
-      time_ago(model.created_at, options)
+      time_ago(created_at: model.created_at, updated_at: model.updated_at, options: options)
     end
 
     private
 
-    def time_ago(time, options = {})
-      time = time.to_time.utc
+    def time_ago(created_at:, updated_at:, options: {})
+      created_at = created_at.to_time.utc
+      updated_at = updated_at.to_time.utc
 
-      options.reverse_merge! title: l(time, format: :short)
+      options.reverse_merge! title: created_updated_tooltip(created_at: created_at, updated_at: updated_at)
 
       options[:data] ||= {}
       options[:data].merge! time_ago: 'moment.js'
 
-      h.time_tag time, time.strftime('%B %e, %Y %l:%M%P'), options
+      h.time_tag created_at, created_at.strftime('%B %e, %Y %l:%M%P'), options
+    end
+
+    # TODO: make sure that note, comment, etc is edited the ActivityObject is touched as well
+     def created_updated_tooltip(created_at: time, updated_at:, format: :short)
+      created_at = l(created_at, format: format)
+      updated_at = l(updated_at, format: format)
+
+      if created_at == updated_at
+        created_at
+      else
+        # add `white-space: pre-wrap;` to .qtip-content
+        "#{created_at}&#10;(edited #{updated_at})".html_safe
+      end
     end
   end
 end
