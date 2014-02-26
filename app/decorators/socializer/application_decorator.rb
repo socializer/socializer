7ed0d7bb22a3ac48d98e77a0ad7abc112db7872b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-26 12:22:20 -0500

    add some documentation comments

--- app/decorators/socializer/application_decorator.rb
+++ app/decorators/socializer/application_decorator.rb
@@ -1,11 +1,18 @@
 module Socializer
   class ApplicationDecorator < Draper::Decorator
+    # @param [Hash] options
+    #
+    # @return [String] An HTML time tag
     def created_at_time_ago(options = {})
       time_ago(created_at: model.created_at, updated_at: model.updated_at, options: options)
     end
 
     private
-
+    # @param [DateTime] created_at
+    # @param [DateTime] updated_at
+    # @param [Hash] options
+    #
+    # @return [String] An HTML time tag
     def time_ago(created_at:, updated_at:, options: {})
       created_at = created_at.to_time.utc
       updated_at = updated_at.to_time.utc
@@ -19,7 +26,13 @@ module Socializer
     end
 
     # TODO: make sure that note, comment, etc is edited the ActivityObject is touched as well
-     def created_updated_tooltip(created_at: time, updated_at:, format: :short)
+    #
+    # @param [DateTime] created_at
+    # @param [DateTime] updated_at
+    # @param [Object] format
+    #
+    # @return [String]
+    def created_updated_tooltip(created_at: time, updated_at:, format: :short)
       created_at = l(created_at, format: format)
       updated_at = l(updated_at, format: format)
 
