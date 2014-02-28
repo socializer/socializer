cfca2fc257872bb11267ac22c53356c7440c1223
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-28 13:11:59 -0500

    add inherited factories with the different activitable associations

--- spec/factories/socializer_activity_objects.rb
+++ spec/factories/socializer_activity_objects.rb
@@ -3,5 +3,25 @@
 FactoryGirl.define do
   factory :socializer_activity_object, class: Socializer::ActivityObject do
     association :activitable, factory: :socializer_note
+
+    factory :socializer_activity_object_activity, class: Socializer::ActivityObject do
+      association :activitable, factory: :socializer_activity
+    end
+
+    factory :socializer_activity_object_circle, class: Socializer::ActivityObject do
+      association :activitable, factory: :socializer_circle
+    end
+
+    factory :socializer_activity_object_comment, class: Socializer::ActivityObject do
+      association :activitable, factory: :socializer_comment
+    end
+
+    factory :socializer_activity_object_group, class: Socializer::ActivityObject do
+      association :activitable, factory: :socializer_group
+    end
+
+    factory :socializer_activity_object_person, class: Socializer::ActivityObject do
+      association :activitable, factory: :socializer_person
+    end
   end
 end
