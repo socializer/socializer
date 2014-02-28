2b462c8062e4102e102ee7a82dcc4ad7bdbe1d8d
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-28 13:46:00 -0500

    use an association for the activity_object

--- spec/factories/socializer_audiences.rb
+++ spec/factories/socializer_audiences.rb
@@ -4,6 +4,6 @@ FactoryGirl.define do
   factory :socializer_audience, class: Socializer::Audience do
     privacy_level 1
     activity_id 1
-    activity_object_id 1
+    association :activity_object, factory: :socializer_activity_object
   end
 end
