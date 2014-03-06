0de554c27e00142fe59dd8bfbc208690f55dfcd8
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-03-05 19:29:48 -0500

    add mass assignment, validations, and additional relationship examples

--- spec/models/socializer/activity_spec.rb
+++ spec/models/socializer/activity_spec.rb
@@ -8,9 +8,29 @@ module Socializer
       expect(activity).to be_valid
     end
 
+    context 'mass assignment' do
+      it { expect(activity).to allow_mass_assignment_of(:verb) }
+      it { expect(activity).to allow_mass_assignment_of(:circles) }
+      it { expect(activity).to allow_mass_assignment_of(:actor_id) }
+      it { expect(activity).to allow_mass_assignment_of(:activity_object_id) }
+      it { expect(activity).to allow_mass_assignment_of(:target_id) }
+    end
+
     context 'relationships' do
+      it { expect(activity).to belong_to(:parent) }
+      it { expect(activity).to belong_to(:activitable_actor) }
+      it { expect(activity).to belong_to(:activitable_object) }
+      it { expect(activity).to belong_to(:activitable_target) }
+      it { expect(activity).to belong_to(:verb) }
+      it { expect(activity).to have_one(:activity_field) }
+      it { expect(activity).to have_many(:audiences) }
+      it { expect(activity).to have_many(:activity_objects) }
+      it { expect(activity).to have_many(:children) }
       it { expect(activity).to have_many(:notifications) }
     end
 
+    context 'validations' do
+      it { expect(activity).to validate_presence_of(:verb) }
+    end
   end
 end
