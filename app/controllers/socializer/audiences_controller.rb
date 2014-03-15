c801d1af678fa1219cd4e11ad8af0ddc9ff60ed8
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-03-15 15:17:35 -0400

    change the scope of the variables

--- app/controllers/socializer/audiences_controller.rb
+++ app/controllers/socializer/audiences_controller.rb
@@ -3,32 +3,32 @@ module Socializer
     before_action :authenticate_user!
 
     def index
-      @people  = []
-      @circles = current_user.circles
-      @groups  = current_user.groups
+      people  = []
+      circles = current_user.circles
+      groups  = current_user.groups
 
       query = params.fetch(:q) { nil }
 
       if query.present? && query.size > 0
         query_value = "%#{query}%"
 
-        @people  = Person.where { display_name.like query_value }
-        @circles = @circles.where { name.like query_value }
-        @groups  = @groups.where { name.like query_value }
+        people  = Person.where { display_name.like query_value }
+        circles = circles.where { name.like query_value }
+        groups  = groups.where { name.like query_value }
       end
 
-      privacy_level = Socializer::Audience.privacy_level
-      public        = privacy_level.find_value(:public)
-      circles       = privacy_level.find_value(:circles)
+      privacy_level         = Socializer::Audience.privacy_level
+      privacy_level_public  = privacy_level.find_value(:public)
+      privacy_level_circles = privacy_level.find_value(:circles)
 
-      @audiences = [id: public.value, name: public.text] +
-                   [id: circles.value, name: circles.text] +
-                   @people.map { |person| { id: person.guid, name: person.display_name } } +
-                   @circles.map { |circle| { id: circle.guid, name: circle.name } } +
-                   @groups.map { |group| { id: group.guid, name: group.name } }
+      audiences = [id: privacy_level_public.value, name: privacy_level_public.text] +
+                  [id: privacy_level_circles.value, name: privacy_level_circles.text] +
+                  people.map { |person| { id: person.guid, name: person.display_name } } +
+                  circles.map { |circle| { id: circle.guid, name: circle.name } } +
+                  groups.map { |group| { id: group.guid, name: group.name } }
 
       respond_to do |format|
-        format.json { render json: @audiences }
+        format.json { render json: audiences }
       end
     end
   end
