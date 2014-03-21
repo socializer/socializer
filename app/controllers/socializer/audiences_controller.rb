2446b288309d2fccca43b5ec143288992c551622
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-03-21 11:06:49 -0400

    prefer select to map. refactor the audiences array

--- app/controllers/socializer/audiences_controller.rb
+++ app/controllers/socializer/audiences_controller.rb
@@ -3,29 +3,34 @@ module Socializer
     before_action :authenticate_user!
 
     def index
-      people  = []
-      circles = current_user.circles
-      groups  = current_user.groups
+      query     = params.fetch(:q) { nil }
 
-      query = params.fetch(:q) { nil }
+      people  = []
+      circles = current_user.circles.select(:name).guids
+      groups  = current_user.groups.select(:name).guids
 
       if query.present? && query.size > 0
         query_value = "%#{query}%"
 
-        people  = Person.where { display_name.like query_value }
-        circles = circles.where { name.like query_value }
-        groups  = groups.where { name.like query_value }
+        people  = Person.select(:display_name).guids.where { display_name.like(query_value) }
+        circles = circles.where { name.like(query_value) }
+        groups  = groups.where { name.like(query_value) }
       end
 
       privacy_level         = Socializer::Audience.privacy_level
       privacy_level_public  = privacy_level.find_value(:public)
-      privacy_level_circles = privacy_level.find_value(:circles)
+      privacy_level_circles = rivacy_level.find_value(:circles)
 
-      audiences = [id: privacy_level_public.value, name: privacy_level_public.text] +
-                  [id: privacy_level_circles.value, name: privacy_level_circles.text] +
-                  people.map { |person| { id: person.guid, name: person.display_name } } +
-                  circles.map { |circle| { id: circle.guid, name: circle.name } } +
-                  groups.map { |group| { id: group.guid, name: group.name } }
+      privacy_public  = { id: privacy_level_public.value, name: privacy_level_public.text }
+      privacy_circles = { id: privacy_level_circles.value, name: privacy_level_circles.text }
+
+      audiences = []
+
+      audiences << privacy_public
+      audiences << privacy_circles
+      audiences.concat(people)
+      audiences.concat(circles)
+      audiences.concat(groups)
 
       respond_to do |format|
         format.json { render json: audiences }
