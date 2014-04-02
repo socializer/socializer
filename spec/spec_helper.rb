957217b84480cd406cff36d34da2c75cad9cedde
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-04-02 12:11:14 -0400

    Put space between the method name and the first argument.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[SimpleCov::Formatter::HTMLFormatter,
+                                                           Coveralls::SimpleCov::Formatter]
 
 SimpleCov.start 'rails'
 
