6ccceb14dbcc31ef7c7bfcd172bea0e42cc796fc
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-04-02 12:09:17 -0400

    Useless private access modifier.

--- app/models/socializer/notification.rb
+++ app/models/socializer/notification.rb
@@ -15,6 +15,7 @@ module Socializer
     validates :activity_id, presence: true
     validates :activity_object_id, presence: true
 
+    # Class methods - Public
     def self.create_for_activity(activity)
       # Get all ties related to the audience of the activity
       potential_contact_id = get_potential_contact_id(activity.id)
@@ -26,32 +27,36 @@ module Socializer
       end
     end
 
-    private
+    class << self
+      # Class methods - Private
 
-    def self.create_notification(activity, contact_id)
-      notification = Notification.new do |n|
-        n.activity = activity
-        n.activity_object = ActivityObject.find_by(id: contact_id)
+      private
+
+      def create_notification(activity, contact_id)
+        notification = Notification.new do |n|
+          n.activity = activity
+          n.activity_object = ActivityObject.find_by(id: contact_id)
+        end
+
+        notification.save!
       end
 
-      notification.save!
-    end
+      def get_potential_contact_id(activity_id)
+        # Activity -> Audience -> ActivityObject -> Circle -> Tie -> contact_id
+        Tie.select { contact_id }
+           .joins  { circle.activity_object.audiences }
+           .where  { circle.activity_object.audiences.activity_id.eq(activity_id) }
+           .flatten.uniq
+      end
 
-    def self.get_potential_contact_id(activity_id)
-      # Activity -> Audience -> ActivityObject -> Circle -> Tie -> contact_id
-      Tie.select { contact_id }
-         .joins  { circle.activity_object.audiences }
-         .where  { circle.activity_object.audiences.activity_id.eq(activity_id) }
-         .flatten.uniq
-    end
-
-    def self.person_in_circle?(parent_contact_id, child_contact_id)
-      # ActivityObject.id = parent_contact_id
-      # ActivityObject -> Circle -> Tie -> contact_id = child_contact_id
-      ActivityObject.select { id }
-                    .joins  { circles.ties }
-                    .where  { id.eq(parent_contact_id) & circles.ties.contact_id.eq(child_contact_id) }
-                    .first.present?
+      def person_in_circle?(parent_contact_id, child_contact_id)
+        # ActivityObject.id = parent_contact_id
+        # ActivityObject -> Circle -> Tie -> contact_id = child_contact_id
+        ActivityObject.select { id }
+                      .joins  { circles.ties }
+                      .where  { id.eq(parent_contact_id) & circles.ties.contact_id.eq(child_contact_id) }
+                      .first.present?
+      end
     end
   end
 end
