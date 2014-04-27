8c7cd42f3fb6adb4dc640d47c0e8fcb2034255e7
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-04-27 10:07:29 -0400

    add the form-control class to vertical forms

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -7,7 +7,7 @@ SimpleForm.setup do |config|
     b.use :html5
     b.use :placeholder
     b.use :label
-    b.use :input
+    b.use :input, class: 'form-control'
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
     b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
