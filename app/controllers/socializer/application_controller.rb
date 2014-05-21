6c2a242978a065441b70fc6f4c0ae77244b62a2f
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-05-20 21:06:15 -0400

    get the test passing again

--- app/controllers/socializer/application_controller.rb
+++ app/controllers/socializer/application_controller.rb
@@ -35,7 +35,7 @@ module Socializer
     end
 
     def extract_locale_from_accept_language_header
-      return if request.blank? && request.env['HTTP_ACCEPT_LANGUAGE'].blank?
+      return unless request.present? && request.env['HTTP_ACCEPT_LANGUAGE'].present?
       request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     end
   end
