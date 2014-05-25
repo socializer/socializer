8694ebcd06f1e9df0be68bba39563c3dd15984e0
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-05-24 20:12:49 -0400

    not part of newly generated engine

--- app/controllers/socializer/application_controller.rb
+++ app/controllers/socializer/application_controller.rb
@@ -1,10 +1,5 @@
 module Socializer
   class ApplicationController < ActionController::Base
-    # Prevent CSRF attacks by raising an exception.
-    # For APIs, you may want to use :null_session instead.
-    # FIXME: Not sure why protect_from_forgery is no longer working
-    # protect_from_forgery with: :exception
-
     helper_method :current_user
     helper_method :signed_in?
 
