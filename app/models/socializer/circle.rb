3f693c908148308dcf6636814f65bd140b584ce2
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-05 12:25:25 -0400

    move the audience_list queries to their respective models

--- app/models/socializer/circle.rb
+++ app/models/socializer/circle.rb
@@ -4,13 +4,23 @@ module Socializer
 
     attr_accessible :name, :content
 
+    # Relationships
     belongs_to :activity_author,  class_name: 'ActivityObject', foreign_key: 'author_id'
 
     has_many   :ties
     has_many   :activity_contacts, through: :ties
 
+    # Validations
     validates :name, presence: true, uniqueness: { scope: :author_id }
 
+    # Class Methods
+    def self.audience_list(current_user, query)
+      @circles ||= current_user.circles.select(:name).guids
+      return @circles if query.blank?
+      @circles ||= @circles.where(Circle.arel_table[:name].matches("%#{query}%"))
+    end
+
+    # Instance Methods
     def author
       @author ||= activity_author.activitable
     end
