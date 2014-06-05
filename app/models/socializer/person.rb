c7086ba105d2d5ce8056e8a7897c755a65d0747f
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-05 12:59:06 -0400

    remove unneeded variable assignment

--- app/models/socializer/person.rb
+++ app/models/socializer/person.rb
@@ -74,24 +74,22 @@ module Socializer
     end
 
     def likes
-      activity_obj_id   = activity_object.id
       verbs_of_interest = %w(like unlike)
       # FIXME: Rails 4.2 - https://github.com/rails/rails/pull/13555 - Allows using relation name when querying joins/includes
-      # query = Activity.joins(:verb).where(actor_id: activity_obj_id).where(target_id: nil).where(verb: { name: verbs_of_interest })
-      query = Activity.joins(:verb).where(actor_id: activity_obj_id).where(target_id: nil).where(socializer_verbs: { name: verbs_of_interest })
+      # query = Activity.joins(:verb).where(actor_id: activity_object.id).where(target_id: nil).where(verb: { name: verbs_of_interest })
+      query = Activity.joins(:verb).where(actor_id: activity_object.id).where(target_id: nil).where(socializer_verbs: { name: verbs_of_interest })
       # Alternate syntax:
-      # query = Activity.joins(:verb).where(actor_id: activity_obj_id).where(target_id: nil).where(verb: Verb.where(name: verbs_of_interest))
+      # query = Activity.joins(:verb).where(actor_id: activity_object.id).where(target_id: nil).where(verb: Verb.where(name: verbs_of_interest))
       @likes ||= query.group(:activity_object_id).having('COUNT(1) % 2 == 1')
     end
 
     def likes?(object)
-      activity_obj_id   = activity_object.id
       verbs_of_interest = %w(like unlike)
       # FIXME: Rails 4.2 - https://github.com/rails/rails/pull/13555 - Allows using relation name when querying joins/includes
-      # query = Activity.joins(:verb).where(activity_object_id: object.id).where(actor_id: activity_obj_id).where(verb: { name: verbs_of_interest })
-      query = Activity.joins(:verb).where(activity_object_id: object.id).where(actor_id: activity_obj_id).where(socializer_verbs: { name: verbs_of_interest })
+      # query = Activity.joins(:verb).where(activity_object_id: object.id).where(actor_id: activity_object.id).where(verb: { name: verbs_of_interest })
+      query = Activity.joins(:verb).where(activity_object_id: object.id).where(actor_id: activity_object.id).where(socializer_verbs: { name: verbs_of_interest })
       # Alternate syntax:
-      # query = Activity.joins(:verb).where(activity_object_id: object.id).where(actor_id: activity_obj_id).where(verb: Verb.where(name: verbs_of_interest))
+      # query = Activity.joins(:verb).where(activity_object_id: object.id).where(actor_id: activity_object.id).where(verb: Verb.where(name: verbs_of_interest))
       query.count.odd?
     end
 
