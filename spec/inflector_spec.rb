e3c512f9278f809bf05661506f272793935560ab
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-08 19:42:33 -0400

    remove should from example names

--- spec/inflector_spec.rb
+++ spec/inflector_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 RSpec.describe ActiveSupport::Inflector::Inflections do
   context 'singularize' do
-    it 'should  return the singular form of a word' do
+    it 'return the singular form of a word' do
       expect('ties'.singularize(:en)).to eq('tie')
       expect('Ties'.singularize(:en)).to eq('Tie')
       expect('activities'.singularize(:en)).to eq('activity')
       expect('Activities'.singularize(:en)).to eq('Activity')
     end
 
-    it 'should not alter an already singular word' do
+    it 'do not alter an already singular word' do
       expect('tie'.singularize(:en)).to eq('tie')
       expect('Tie'.singularize(:en)).to eq('Tie')
       expect('activity'.singularize(:en)).to eq('activity')
