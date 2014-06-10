268e962a33d1786ded1b8897efcff4672f2f898a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-10 12:38:01 -0400

    test the current_user? helper method

--- spec/helpers/socializer/application_helper_spec.rb
+++ spec/helpers/socializer/application_helper_spec.rb
@@ -7,5 +7,24 @@ module Socializer
         expect(helper.signin_path(:facebook)).to eq('/auth/facebook')
       end
     end
+
+    context '#current_user?' do
+      let(:person) { build(:socializer_person) }
+      context 'is false' do
+        before do
+          allow(helper).to receive(:current_user).and_return(nil)
+        end
+
+        it { expect(helper.current_user?(person)).to be false }
+      end
+
+      context 'is true' do
+        before do
+          allow(helper).to receive(:current_user).and_return(person)
+        end
+
+        it { expect(helper.current_user?(person)).to be true }
+      end
+    end
   end
 end
