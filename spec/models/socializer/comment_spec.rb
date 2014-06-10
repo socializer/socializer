291f7cb5ccb75717cea2e5c6a8e3245e1cc7c85a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-10 14:35:12 -0400

    test content, mass assignment, and relationships

--- spec/models/socializer/comment_spec.rb
+++ spec/models/socializer/comment_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 module Socializer
   RSpec.describe Comment, type: :model do
-    let(:comment) { build(:socializer_comment) }
+    let(:comment) { build(:socializer_comment, content: 'Comment') }
 
     it 'has a valid factory' do
       expect(comment).to be_valid
     end
 
-    it { is_expected.to respond_to(:content) }
-    it { is_expected.to respond_to(:author) }
+    context 'mass assignment' do
+      it { is_expected.to allow_mass_assignment_of(:content) }
+    end
+
+    context 'relationships' do
+      it { is_expected.to belong_to(:activity_author) }
+    end
 
     # TODO: Test return values
     it { expect(comment.author).to be_kind_of(Socializer::Person) }
+    it { expect(comment.content).to eq('Comment') }
   end
 end
