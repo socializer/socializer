3a16747268151e850941cc16d1ef16142c42ef01
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-12 13:54:13 -0400

    remove completed TODO

--- spec/factories/socializer_activities.rb
+++ spec/factories/socializer_activities.rb
@@ -1,6 +1,5 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-# TODO: Redo this factory once the other factories are added
 FactoryGirl.define do
   factory :socializer_activity, class: Socializer::Activity do
     association :activity_object, factory: :socializer_activity_object
