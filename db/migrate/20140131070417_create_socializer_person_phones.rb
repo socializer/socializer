6fd071b1a66f3278e18623f625215ae1b4eafd65
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-12 17:58:16 -0400

    all null: false to category, number, and label

--- db/migrate/20140131070417_create_socializer_person_phones.rb
+++ db/migrate/20140131070417_create_socializer_person_phones.rb
@@ -2,13 +2,10 @@ class CreateSocializerPersonPhones < ActiveRecord::Migration
   def change
     create_table :socializer_person_phones do |t|
       t.integer  :person_id, null: false
-      # TODO: add null: false
-      t.integer  :category
-      # TODO: change label to name
-      # TODO: add null: false
-      t.integer  :label
-      # TODO: add null: false
-      t.string   :number
+      t.integer  :category,  null: false
+      # TODO: What's label for
+      t.integer  :label,     null: false
+      t.string   :number,    null: false
 
       t.timestamps
     end
