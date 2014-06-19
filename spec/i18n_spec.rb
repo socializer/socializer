a30f3f2123741d5186ceea5c8c4da10fb0f7cc3a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-19 12:29:31 -0400

    add an i18n spec
    this uses i18n-tasks

---
+++ spec/i18n_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'i18n/tasks'
+
+RSpec.describe 'I18n' do
+  let(:i18n) { I18n::Tasks::BaseTask.new }
+
+  it 'does not have missing keys' do
+    count = i18n.missing_keys.count
+    # fail "There are #{count} missing i18n keys! Run 'i18n-tasks missing' for more details." unless count.zero?
+    warn "There are #{count} missing i18n keys! Run 'i18n-tasks missing' for more details." unless count.zero?
+  end
+
+  it 'does not have unused keys' do
+    count = i18n.unused_keys.count
+    fail "There are #{count} unused i18n keys! Run 'i18n-tasks unused' for more details." unless count.zero?
+  end
+end
