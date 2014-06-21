cb66e6085648c209363ecd5889f92f9ed04a756d
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-06-21 11:43:57 -0400

    use symbol instead of integer

--- spec/factories/socializer_audiences.rb
+++ spec/factories/socializer_audiences.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :socializer_audience, class: Socializer::Audience do
-    privacy_level 1
+    privacy_level :public
     activity_id 1
     association :activity_object, factory: :socializer_activity_object
   end
