489004165524c6052d3b36dc46d66aa8b93f3a7b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-07-06 15:49:02 -0400

    create the person decorator with the birthday and toolbar_stream_links methods

---
+++ app/decorators/socializer/person_decorator.rb
@@ -0,0 +1,71 @@
+module Socializer
+  class PersonDecorator < ApplicationDecorator
+    delegate_all
+
+    # Define presentation-specific methods here. Helpers are accessed through
+    # `helpers` (aka `h`). You can override attributes, for example:
+    #
+    #   def created_at
+    #     helpers.content_tag :span, class: 'time' do
+    #       object.created_at.strftime("%a %m/%d/%y")
+    #     end
+    #   end
+
+    def birthday
+      model.birthdate? ? model.birthdate.to_s(:long_ordinal) : nil
+    end
+
+    def toolbar_stream_links
+      list = model.circles + model.memberships
+      return if list.blank?
+
+      html = []
+
+      list[0..2].map do |item|
+        html << toolbar_link(item)
+      end.join.html_safe
+
+      html << helpers.content_tag(:li, class: 'dropdown') do
+        dropdown_link +
+
+        helpers.content_tag(:ul, class: 'dropdown-menu') do
+          list[3..(list.size)].map do |item|
+            toolbar_link(item)
+          end.join.html_safe
+        end
+      end
+
+      html.join.html_safe
+    end
+
+    private
+
+    def dropdown_link
+      icon = helpers.content_tag(:span, nil, class: 'fa fa-angle-down fa-fw')
+      # TODO: add a translation for More
+      helpers.link_to('#', class: 'dropdown-toggle', data: { toggle: 'dropdown' }) do
+        "More #{icon}".html_safe
+      end
+    end
+
+    def toolbar_link(item)
+      if item.class.name == 'Socializer::Circle'
+        provider = :circles
+        item_id  = item.id
+        item_name = item.name
+      end
+
+      if item.class.name == 'Socializer::Membership'
+        provider  = :groups
+        item_id   = item.group.id
+        item_name = item.group.name
+      end
+
+      class_name = 'active' if helpers.current_page?(helpers.stream_path(provider: provider, id: item_id))
+
+      helpers.content_tag(:li) do
+        helpers.link_to(item_name, helpers.stream_path(provider: provider, id: item_id), class: class_name)
+      end
+    end
+  end
+end
--- spec/decorators/socializer/person_decorator_spec.rb
+++ spec/decorators/socializer/person_decorator_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
+include Socializer::Engine.routes.url_helpers
 
 module Socializer
-  RSpec.describe PersonDecorator, type: :decorator do
-    context 'birthdate' do
-      let(:person) { build(:socializer_person) }
-      let(:decorated_person) { PersonDecorator.new(person) }
+  RSpec.describe PersonDecorator, :draper_with_helpers, type: :decorator do
+    let(:person) { build(:socializer_person) }
+    let(:decorated_person) { PersonDecorator.new(person) }
 
+    context 'birthdate' do
       context 'not specified' do
         it { expect(decorated_person.birthday).to be nil }
       end
@@ -13,7 +14,61 @@ module Socializer
       context 'specified' do
         let(:person) { build(:socializer_person, birthdate: Time.now - 10.years) }
 
-        it { expect(decorated_person.birthday).to eq person.birthdate.to_s(:long_ordinal) }
+        it { expect(decorated_person.birthday).to eq(person.birthdate.to_s(:long_ordinal)) }
+      end
+    end
+
+    context 'toolbar_stream_links' do
+      let(:person) { create(:socializer_person) }
+      let(:decorated_person) { PersonDecorator.new(person) }
+
+      context 'with no circles or memberships' do
+        it { expect(decorated_person.toolbar_stream_links).to eq(nil) }
+      end
+
+      context 'with circles and no memberships' do
+        before do
+          person.add_default_circles
+        end
+
+        let(:result) do
+          decorated_person.toolbar_stream_links
+        end
+
+        it { expect(result.html_safe?).to be true }
+        it { expect(result).to have_link('Friends', href: '/stream/circles/1') }
+        it { expect(result).to have_link('Family', href: '/stream/circles/2') }
+        it { expect(result).to have_link('Acquaintances', href: '/stream/circles/3') }
+        it { expect(result).to have_link('Following', href: '/stream/circles/4') }
+        it { expect(result).not_to have_link('Group', href: '/stream/groups/1') }
+
+        it { expect(result).to have_link('More', href: '#') }
+        it { expect(result).to have_selector('li.dropdown') }
+        it { expect(result).to have_selector('a.dropdown-toggle') }
+        it { expect(result).to have_selector('ul.dropdown-menu') }
+      end
+
+      context 'with no circles, but with memberships' do
+        before do
+          person.add_default_circles
+          create(:socializer_group, author_id: person.id, name: 'Group')
+        end
+
+        let(:result) do
+          decorated_person.toolbar_stream_links
+        end
+
+        it { expect(result.html_safe?).to be true }
+        it { expect(result).to have_link('Friends', href: '/stream/circles/1') }
+        it { expect(result).to have_link('Family', href: '/stream/circles/2') }
+        it { expect(result).to have_link('Acquaintances', href: '/stream/circles/3') }
+        it { expect(result).to have_link('Following', href: '/stream/circles/4') }
+        it { expect(result).to have_link('Group', href: '/stream/groups/1') }
+
+        it { expect(result).to have_link('More', href: '#') }
+        it { expect(result).to have_selector('li.dropdown') }
+        it { expect(result).to have_selector('a.dropdown-toggle') }
+        it { expect(result).to have_selector('ul.dropdown-menu') }
       end
     end
   end
