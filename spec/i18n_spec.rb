47abb8e67c9176bf8a63917f06841c768fc9fc4b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-07-15 13:38:46 -0400

    update the spec for the latest version of i18n-tasks

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -3,16 +3,16 @@ require 'i18n/tasks'
 
 RSpec.describe 'I18n' do
   let(:i18n) { I18n::Tasks::BaseTask.new }
+  let(:missing_keys) { i18n.missing_keys }
+  let(:unused_keys) { i18n.unused_keys }
 
   it 'does not have missing keys' do
-    count = i18n.missing_keys.count
-    # fail "There are #{count} missing i18n keys! Run 'i18n-tasks missing' for more details." unless count.zero?
-    warn "There are #{count} missing i18n keys! Run 'i18n-tasks missing' for more details." unless count.zero?
+    expect(missing_keys).to be_empty,
+                            "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
 
   it 'does not have unused keys' do
-    count = i18n.unused_keys.count
-    # fail "There are #{count} unused i18n keys! Run 'i18n-tasks unused' for more details." unless count.zero?
-    warn "There are #{count} unused i18n keys! Run 'i18n-tasks unused' for more details." unless count.zero?
+    expect(i18n.unused_keys).to be_empty,
+                                "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
   end
 end
