7df50839da2fb07bbc81c652280088d762956a9f
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-07-22 14:28:35 -0400

    uncommunicative variable name

--- lib/socializer/engine.rb
+++ lib/socializer/engine.rb
@@ -22,16 +22,16 @@ module Socializer
   class Engine < Rails::Engine
     isolate_namespace Socializer
 
-    config.generators do |g|
-      g.test_framework :rspec,
+    config.generators do |generator|
+      generator.test_framework :rspec,
                        fixtures: true,
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: true,
                        controller_specs: true,
                        request_specs: false
-      g.integration_tool    false
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      generator.integration_tool    false
+      generator.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
 end
