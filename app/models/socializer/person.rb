5d5da1a4a70c521f4d017355283dc25b04359319
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-07-23 11:22:23 -0400

    use the circles delegate

--- app/models/socializer/person.rb
+++ app/models/socializer/person.rb
@@ -214,10 +214,10 @@ module Socializer
 
     # Add the default circles for the current person
     def add_default_circles
-      activity_object.circles.create!(display_name: 'Friends',       content: 'Your real friends, the ones you feel comfortable sharing private details with.')
-      activity_object.circles.create!(display_name: 'Family',        content: 'Your close and extended family, with as many or as few in-laws as you want.')
-      activity_object.circles.create!(display_name: 'Acquaintances', content: "A good place to stick people you've met but aren't particularly close to.")
-      activity_object.circles.create!(display_name: 'Following',     content: "People you don't know personally, but whose posts you find interesting.")
+      circles.create!(display_name: 'Friends',       content: 'Your real friends, the ones you feel comfortable sharing private details with.')
+      circles.create!(display_name: 'Family',        content: 'Your close and extended family, with as many or as few in-laws as you want.')
+      circles.create!(display_name: 'Acquaintances', content: "A good place to stick people you've met but aren't particularly close to.")
+      circles.create!(display_name: 'Following',     content: "People you don't know personally, but whose posts you find interesting.")
     end
 
     private
