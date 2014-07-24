d5d516da49f8b0bef6c09427a9d75e4616374480
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-07-24 17:39:46 -0400

    don't pass created_at and updated_at around
    access them from model

--- app/decorators/socializer/application_decorator.rb
+++ app/decorators/socializer/application_decorator.rb
@@ -7,44 +7,40 @@ module Socializer
     #
     # @param options [Hash]
     #
-    # @return [String]
+    # @return [String] An HTML time tag
     def created_at_time_ago(options = {})
-      time_ago(created_at: model.created_at, updated_at: model.updated_at, options: options)
+      time_ago(options: options)
     end
 
     private
 
     # Builds an HTML time tag
     #
-    # @param created_at [DateTime]
-    # @param updated_at [DateTime]
     # @param options [Hash]
     #
     # @return [String] An HTML time tag
-    def time_ago(created_at:, updated_at:, options: {})
-      created_at = created_at.to_time.utc
-      updated_at = updated_at.to_time.utc
+    def time_ago(options: {})
+      created_at = model.created_at.to_time.utc
 
-      options.reverse_merge! title: created_updated_tooltip(created_at: created_at, updated_at: updated_at)
+      options.reverse_merge! title: created_updated_tooltip_text
 
       options[:data] ||= {}
       options[:data].merge! time_ago: 'moment.js'
 
-      h.time_tag created_at, created_at.strftime('%B %e, %Y %l:%M%P'), options
+      h.time_tag(created_at, created_at.strftime('%B %e, %Y %l:%M%P'), options)
     end
 
-    # TODO: make sure that note, comment, etc is edited the ActivityObject is touched as well
+    # TODO: make sure that note, comment, etc is edited when the ActivityObject is touched
+    #
     # Creates the title/tooltip. If the model has been updated, it returns a multiline string.
     # If not, it returns the created_at value
     #
-    # @param created_at [DateTime]
-    # @param updated_at [DateTime]
     # @param format [Symbol]
     #
     # @return [String]
-    def created_updated_tooltip(created_at: time, updated_at:, format: :short)
-      created_at = l(created_at, format: format)
-      updated_at = l(updated_at, format: format)
+    def created_updated_tooltip_text(format: :short)
+      created_at = l(model.created_at.to_time.utc, format: format)
+      updated_at = l(model.updated_at.to_time.utc, format: format)
 
       if created_at == updated_at
         created_at
