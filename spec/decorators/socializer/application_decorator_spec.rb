fb405ceb398e64b6a12ae9fff488922bb705222c
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-07-25 19:41:09 -0400

    assign the return value of created_at_time_ago  to result

--- spec/decorators/socializer/application_decorator_spec.rb
+++ spec/decorators/socializer/application_decorator_spec.rb
@@ -9,11 +9,12 @@ module Socializer
       let(:updated_at) { activity.updated_at.to_time.utc }
 
       context 'when created_at and updated_at are equal' do
+        let(:result) { decorated_activity.created_at_time_ago }
         let(:time_tag) do
           %Q(<time data-time-ago="moment.js" datetime="#{created_at.iso8601}" title="#{created_at.to_s(:short)}">#{created_at.strftime('%B %e, %Y %l:%M%P')}</time>)
         end
 
-        it { expect(decorated_activity.created_at_time_ago).to eq(time_tag) }
+        it { expect(result).to eq(time_tag) }
       end
 
       context 'when created_at and updated_at are different' do
@@ -22,11 +23,12 @@ module Socializer
           decorated_activity.save!
         end
 
+        let(:result) { decorated_activity.created_at_time_ago }
         let(:time_tag) do
           %Q(<time data-time-ago="moment.js" datetime="#{created_at.iso8601}" title="#{created_at.to_s(:short)}&#10;(edited #{updated_at.to_s(:short)})">#{created_at.strftime('%B %e, %Y %l:%M%P')}</time>)
         end
 
-        it { expect(decorated_activity.created_at_time_ago).to eq(time_tag) }
+        it { expect(result).to eq(time_tag) }
       end
     end
   end
