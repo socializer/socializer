754f739fe2a1919951317d77235a24747cac456e
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-08-08 11:50:26 -0400

    create the AudienceList service
    Move the audience list methods from the audience model to the new service

--- app/models/socializer/audience.rb
+++ app/models/socializer/audience.rb
@@ -19,23 +19,6 @@ module Socializer
 
     # Class Methods
 
-    # Build the audience list that is used in the notes and shares forms
-    #
-    # @example
-    # Audience.audience_list(current_user, query)
-    #
-    # @param current_user [Socializer::Person] The currently logged in user
-    # @param query [String] Used to filter the audience list. Can be nil
-    #
-    # @return [Array] [description]
-    def self.audience_list(current_user, query)
-      people  = query.blank? ? [] : Person.audience_list(query)
-      circles = current_user.audience_list(:circles, query)
-      groups  = current_user.audience_list(:groups, query)
-
-      build_audience_list_array(OpenStruct.new(people: people, circles: circles, groups: groups))
-    end
-
     # This method is a shorthand for the enumerize find_value(value).value method(s)
     #
     # @param privacy: [String]
@@ -46,45 +29,9 @@ module Socializer
       self.privacy.find_value(privacy).value
     end
 
-    # Returns a {Hash} containing the value and text for the privacy level
-    #
-    # @example
-    #   privacy_hash(:public)
-    #
-    # @param  privacy_symbol [Symbol] The symbol representing the Audience privacy
-    #
-    # @return [Hash] Using the example you will get !{id: 1, name: 'Public'}
-    def self.privacy_hash(privacy_symbol)
-      privacy_symbol = privacy_symbol.downcase.to_sym
-      privacy        = Audience.privacy.find_value(privacy_symbol)
-
-      { id: privacy.value, name: privacy.text }
-    end
-
     # Instance Methods
     def object
       @object ||= activity_object.activitable
     end
-
-    # Class Methods - Private
-    def self.build_audience_list_array(audience_list)
-      audiences = []
-
-      audiences << merge_icon(Audience.privacy_hash(:public), 'fa-globe')
-      audiences << merge_icon(Audience.privacy_hash(:circles), 'fa-google-circles')
-
-      # TODO: may use the avatar for the user
-      audiences.concat(merge_icon(audience_list.people, 'fa-user'))
-      audiences.concat(merge_icon(audience_list.circles, 'fa-google-circles'))
-      audiences.concat(merge_icon(audience_list.groups, 'fa-users'))
-    end
-    private_class_method :build_audience_list_array
-
-    def self.merge_icon(list, icon)
-      return list.merge(icon: icon) if list.is_a?(Hash)
-      list = list.to_a unless list.is_a?(Array)
-      list.map { |item| item.serializable_hash.merge(icon: icon) }
-    end
-    private_class_method :merge_icon
   end
 end
---
+++ app/services/socializer/audience_list.rb
@@ -0,0 +1,67 @@
+#
+# Namespace for the Socializer engine
+#
+module Socializer
+  #
+  # Build the audience list that is used in the notes and shares forms
+  #
+  class AudienceList
+    # Initializer
+    #
+    # @param person: [Socializer:Person] the person to build the list for
+    # @param query: nil [String] Used to filter the audience list.
+    #
+    # @return [Socializer:AudienceList] returns an instance of AudienceList
+    def initialize(person:, query: nil)
+      @person = person
+      @query  = query
+    end
+
+    # Create the audience list
+    #
+    # @return [Array]
+    def perform
+      # "Hello #{@person.display_name} with query '#{@query}'"
+      people  = @query.blank? ? [] : Person.audience_list(@query)
+      circles = @person.audience_list(:circles, @query)
+      groups  = @person.audience_list(:groups, @query)
+
+      build_audience_list_array(OpenStruct.new(people: people, circles: circles, groups: groups))
+    end
+
+    private
+
+    # Returns a {Hash} containing the value and text for the privacy level
+    #
+    # @example
+    #   privacy_hash(:public)
+    #
+    # @param  privacy_symbol [Symbol] The symbol representing the Audience privacy
+    #
+    # @return [Hash] Using the example you will get !{id: 1, name: 'Public'}
+    def privacy_hash(privacy_symbol)
+      privacy_symbol = privacy_symbol.downcase.to_sym
+      privacy        = Audience.privacy.find_value(privacy_symbol)
+
+      { id: privacy.value, name: privacy.text }
+    end
+
+    def build_audience_list_array(audience_list)
+      audiences = []
+
+      audiences << merge_icon(privacy_hash(:public), 'fa-globe')
+      audiences << merge_icon(privacy_hash(:circles), 'fa-google-circles')
+
+      # TODO: may use the avatar for the user
+      audiences.concat(merge_icon(audience_list.people, 'fa-user'))
+      audiences.concat(merge_icon(audience_list.circles, 'fa-google-circles'))
+      audiences.concat(merge_icon(audience_list.groups, 'fa-users'))
+    end
+
+    def merge_icon(list, icon)
+      return list.merge(icon: icon) if list.is_a?(Hash)
+      list = list.to_a unless list.is_a?(Array)
+      list.map { |item| item.serializable_hash.merge(icon: icon) }
+    end
+  end
+end
