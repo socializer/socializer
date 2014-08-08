31fc47a92cd0ed0c76255946a4ed92a1d4411553
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-08-08 12:24:41 -0400

    use the perform class method

--- app/controllers/socializer/audiences_controller.rb
+++ app/controllers/socializer/audiences_controller.rb
@@ -7,7 +7,7 @@ module Socializer
 
     def index
       query     = params.fetch(:q) { nil }
-      audiences = AudienceList.new(person: current_user, query: query).perform
+      audiences = AudienceList.perform(person: current_user, query: query)
 
       respond_to do |format|
         format.json { render json: audiences }
