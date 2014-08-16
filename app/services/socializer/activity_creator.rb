bc4899595a3ad40d9f34eb888c22ae5865487ee2
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-08-16 16:44:24 -0400

    include ActiveModel::Model
    Avoid parameter lists longer than 5 parameters.
    Make use of validations

--- app/services/socializer/activity_creator.rb
+++ app/services/socializer/activity_creator.rb
@@ -6,6 +6,17 @@ module Socializer
   # Service object for creating and Socializer::Activity
   #
   class ActivityCreator
+    include ActiveModel::Model
+
+    attr_accessor :actor_id, :activity_object_id, :target_id, :verb, :object_ids, :content
+
+    # Validations
+    validates :actor_id, presence: true
+    validates :activity_object_id, presence: true
+    validates :verb, presence: true
+
+    # Instance Methods
+
     # Creates an activity, adds the content and audience if needed.
     #
     # @param actor_id [Integer]
@@ -16,7 +27,7 @@ module Socializer
     # @param content [String] Text with the share
     #
     # @return [OpenStruct]
-    def self.create!(actor_id:, activity_object_id:, target_id: nil, verb:, object_ids: nil, content: nil)
+    def perform
       object = Activity.create! do |activity|
         activity.actor_id           = actor_id
         activity.activity_object_id = activity_object_id
@@ -30,13 +41,14 @@ module Socializer
       OpenStruct.new(activity: object, success?: object.persisted?)
     end
 
+    private
+
     # Add an audience to the activity
     #
     # @param activity: [Socializer::Activity] The activity to add the audience to
     # @param object_ids [Array<Integer>] List of audiences to target
-    def self.add_audience_to_activity(activity:, audience_ids:)
+    def add_audience_to_activity(activity:, audience_ids:)
       audience_ids = audience_ids.split(',') if %w(Fixnum String).include?(audience_ids.class.name)
-      privacy      = Audience.privacy
       limited      = Audience.privacy_value(privacy: :limited)
       not_limited  = %W(#{Audience.privacy_value(privacy: :public)} #{Audience.privacy_value(privacy: :circles)})
 
