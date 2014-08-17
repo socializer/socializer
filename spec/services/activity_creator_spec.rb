471faeba2d33618cbcbce94ef8e72d6d510cbf31
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-08-17 15:01:05 -0400

    add a few more examples for ActivityCreator

--- spec/services/activity_creator_spec.rb
+++ spec/services/activity_creator_spec.rb
@@ -2,10 +2,31 @@ require 'rails_helper'
 
 module Socializer
   RSpec.describe ActivityCreator, type: :service do
+    let(:ac) { ActivityCreator.new }
+
     context 'validations' do
       it { is_expected.to validate_presence_of(:actor_id) }
       it { is_expected.to validate_presence_of(:activity_object_id) }
       it { is_expected.to validate_presence_of(:verb) }
+
+      it { expect(ac.valid?).to be false }
+    end
+
+    context '.perform' do
+      context 'with no required attributes it should raise ActivityCreator::RecordInvalid' do
+        it { expect { ac.perform }.to raise_error(ActivityCreator::RecordInvalid) }
+      end
+
+      context 'with the required attributes' do
+        let(:person) { create(:socializer_person) }
+        let(:activity_object) { create(:socializer_activity_object) }
+        let(:ac) { ActivityCreator.new(actor_id: person.id, activity_object_id: activity_object.id, verb: 'post') }
+
+        it { expect(ac.valid?).to be true }
+        it { expect(ac.perform).to be_kind_of(OpenStruct) }
+        it { expect(ac.perform.activity).to be_kind_of(Activity) }
+        it { expect(ac.perform.success?).to be true }
+      end
     end
   end
 end
