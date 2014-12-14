ee07b42ca3073a2ff8821d06786ed6a9c771f51d
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-12-14 14:22:43 -0500

    need to include the table name for this to work in 4.2.0.rc3

--- app/models/socializer/activity.rb
+++ app/models/socializer/activity.rb
@@ -101,7 +101,7 @@ module Socializer
     # FIXME: Should display notes even if circle has no members and the owner is viewing it.
     #        Notes still don't show after adding people to the circles.
     def self.circle_stream(actor_uid:, viewer_id:)
-      circles  = Circle.select(:id).where(id: actor_uid, author_id: viewer_id)
+      circles  = Circle.select(:id).where(socializer_circles: { id: actor_uid, author_id: viewer_id })
       followed = Tie.select(:contact_id).where(circle_id: circles)
 
       stream_query(viewer_id: viewer_id).where(socializer_activities: { actor_id: followed }).distinct
