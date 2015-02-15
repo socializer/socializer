3aba73d8693f4992a7cb06ec60426e7a4ff191d9
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-02-14 19:42:00 -0500

    add the 'with an audience' context
    check when the audience is ‘public’ and circles

--- spec/services/activity_audience_list_spec.rb
+++ spec/services/activity_audience_list_spec.rb
@@ -28,6 +28,31 @@ module Socializer
         it { expect(audience_list.size).to eq(1) }
         it { expect(audience_list.first).to start_with('name') }
       end
+
+      context 'with an audience' do
+        let(:person) { create(:socializer_person) }
+        let(:note) { person.activity_object.notes.create!(content: 'Test note', object_ids: ['public'], activity_verb: 'post') }
+        let(:activity) { Activity.find_by(activity_object_id: note.activity_object.id) }
+
+        context 'public' do
+          let(:audience_list) { ActivityAudienceList.new(activity: activity).perform }
+
+          it { expect(audience_list.size).to eq(1) }
+          it { expect(audience_list.first).to eq(I18n.t('socializer.activities.audiences.index.tooltip.public')) }
+        end
+
+        context 'that is circles' do
+          before :each do
+            AddDefaultCircles.perform(person: person)
+          end
+
+          let(:note) { person.activity_object.notes.create!(content: 'Test note', object_ids: ['circles'], activity_verb: 'post') }
+          let(:activity) { Activity.find_by(activity_object_id: note.activity_object.id) }
+          let(:audience_list) { ActivityAudienceList.new(activity: activity).perform }
+
+          it { expect(audience_list.size).to eq(1) }
+          it { expect(audience_list.first).to start_with('name') }
+        end
     end
   end
 end
