140da2231f7c5b8a879e8c7c356472c6c917f32a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-03-17 20:06:12 -0400

    change indenttion

--- app/models/socializer/activity_object.rb
+++ app/models/socializer/activity_object.rb
@@ -25,10 +25,10 @@ module Socializer
     has_many :object_activities, class_name: 'Activity', foreign_key: 'activity_object_id', dependent: :destroy
     has_many :target_activities, class_name: 'Activity', foreign_key: 'target_id', dependent: :destroy
 
-    has_many :notes,      foreign_key: 'author_id', inverse_of: :activity_author
-    has_many :comments,   foreign_key: 'author_id', inverse_of: :activity_author
-    has_many :groups,     foreign_key: 'author_id', inverse_of: :activity_author
-    has_many :circles,    foreign_key: 'author_id', inverse_of: :activity_author
+    has_many :notes,    foreign_key: 'author_id', inverse_of: :activity_author
+    has_many :comments, foreign_key: 'author_id', inverse_of: :activity_author
+    has_many :groups,   foreign_key: 'author_id', inverse_of: :activity_author
+    has_many :circles,  foreign_key: 'author_id', inverse_of: :activity_author
 
     has_many :ties,       foreign_key: 'contact_id', inverse_of: :activity_contact
     has_many :memberships, -> { Membership.active }, foreign_key: 'member_id', inverse_of: :activity_member
