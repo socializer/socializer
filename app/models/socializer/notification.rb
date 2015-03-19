608f7287827c916bf5cbb7735f8e6b684ef56758
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-03-19 13:19:08 -0400

    use required keyword arguments

--- app/models/socializer/notification.rb
+++ app/models/socializer/notification.rb
@@ -29,12 +29,12 @@ module Socializer
     # @return [Array] an [Array] of [Socializer::Notification] objects
     def self.create_for_activity(activity)
       # Get all ties related to the audience of the activity
-      potential_contact_ids = get_potential_contact_ids(activity.id)
+      potential_contact_ids = get_potential_contact_ids(activity_id: activity.id)
 
       potential_contact_ids.each do |tie|
-        next unless person_in_circle?(tie.contact_id, activity.activitable_actor.id)
+        next unless person_in_circle?(parent_contact_id: tie.contact_id, child_contact_id: activity.activitable_actor.id)
         # If the contact has the author of the activity in one of his circle.
-        create_notification(activity, tie.contact_id)
+        create_notification(activity: activity, contact_id: tie.contact_id)
       end
     end
 
@@ -62,7 +62,7 @@ module Socializer
     # @param contact_id [Socializer::Person]
     #
     # @return [Socializer::Notification]
-    def self.create_notification(activity, contact_id)
+    def self.create_notification(activity:, contact_id:)
       object = Notification.new do |notification|
         notification.activity = activity
         notification.activity_object = ActivityObject.find_by(id: contact_id)
@@ -73,7 +73,7 @@ module Socializer
     private_class_method :create_notification
 
     # FIXME: Move to Tie or Activity
-    def self.get_potential_contact_ids(activity_id)
+    def self.get_potential_contact_ids(activity_id:)
       # Activity -> Audience -> ActivityObject -> Circle -> Tie -> contact_id
       Tie.select(:contact_id)
          .joins(circle: { activity_object: :audiences })
@@ -83,7 +83,7 @@ module Socializer
     private_class_method :get_potential_contact_ids
 
     # FIXME: Move to ActivityObject or Circle
-    def self.person_in_circle?(parent_contact_id, child_contact_id)
+    def self.person_in_circle?(parent_contact_id:, child_contact_id:)
       # ActivityObject.id = parent_contact_id
       # ActivityObject -> Circle -> Tie -> contact_id = child_contact_id
       ActivityObject.select(:id)
