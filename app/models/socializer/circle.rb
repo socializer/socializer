e5610bde61a0d2ed62c5bf8ccd06b1caf80184a8
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-03-30 11:53:29 -0400

    add some documentation to the remove_contact method

--- app/models/socializer/circle.rb
+++ app/models/socializer/circle.rb
@@ -47,6 +47,13 @@ module Socializer
       ties.create!(contact_id: contact_id)
     end
 
+    # Remove a contact from the circle
+    #
+    # @param contact_id [Integer] The guid of the person to add to the circle
+    #
+    # @return [Socializer:Circle/FalseClass] Deletes the record in the database and freezes this instance to reflect
+    # that no changes should be made (since they can't be persisted). If the before_destroy callback returns false
+    # the action is cancelled and remove_contact returns false.
     def remove_contact(contact_id)
       tie = ties.find_by(contact_id: contact_id)
       tie.destroy
