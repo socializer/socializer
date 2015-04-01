601472e7519ba3c5a0e31afe6c4b7b2780927670
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-01 12:14:01 -0400

    add the person_id and provider indexes

--- db/migrate/20110804192211_create_socializer_authentications.rb
+++ db/migrate/20110804192211_create_socializer_authentications.rb
@@ -8,5 +8,8 @@ class CreateSocializerAuthentications < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    add_index :socializer_authentications, :person_id
+    add_index :socializer_authentications, :provider
   end
 end
