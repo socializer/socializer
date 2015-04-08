7ddbee79cccd9910c73309aec20654cd887ed53c
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-07 20:07:05 -0400

    fix alignment issue

--- app/controllers/socializer/activities/shares_controller.rb
+++ app/controllers/socializer/activities/shares_controller.rb
@@ -15,8 +15,8 @@ module Socializer
         share = params[:share]
         activity_object = ActivityObject.find_by(id: share[:activity_id])
         activity_object.share(actor_id: current_user.guid,
-                               object_ids: share[:object_ids].split(','),
-                               content: share[:content])
+                              object_ids: share[:object_ids].split(','),
+                              content: share[:content])
 
         flash[:notice] = t('socializer.model.share', model: activity_object.activitable_type.demodulize)
         redirect_to activities_path
