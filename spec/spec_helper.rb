106543d7a9870751ad3bc15a9818080d19bc3a5b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-10 10:59:42 -0400

    update the comment for mocks.verify_partial_doubles

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -82,7 +82,8 @@ RSpec.configure do |config|
     mocks.syntax = :expect
 
     # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended.
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
     # mocks.verify_partial_doubles = true
     # TODO: Remove the below and uncomment the above once allow(helper).to receive(:current_user) acts on a real object
     mocks.patch_marshal_to_support_partial_doubles = false
