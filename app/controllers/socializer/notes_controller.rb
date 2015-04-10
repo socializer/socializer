e00a620ebf9a75e97ec22ccde9d5876ae187dce9
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-10 10:14:12 -0400

    document to the notes controller actions

--- app/controllers/socializer/notes_controller.rb
+++ app/controllers/socializer/notes_controller.rb
@@ -6,11 +6,13 @@ module Socializer
     before_action :authenticate_user
     before_action :set_note, only: [:edit, :update, :destroy]
 
+    # GET /notes/new
     def new
       @note = Note.new
       @current_id = params[:id]
     end
 
+    # POST /notes
     def create
       note      = create_note
       @activity = Activity.find_by(activity_object_id: note.guid).decorate
@@ -28,9 +30,11 @@ module Socializer
       end
     end
 
+    # GET /notes/1/edit
     def edit
     end
 
+    # PATCH/PUT /notes/1
     def update
       @note.update!(params[:note])
 
@@ -38,6 +42,7 @@ module Socializer
       redirect_to activities_path
     end
 
+    # DELETE /notes/1
     def destroy
       @activity_guid = Activity.find_by(activity_object_id: @note.guid).guid
       @note.destroy
