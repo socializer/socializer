efbf3427d8593da27dfd22f4ae5a917e6b437277
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-20 19:40:30 -0400

    routing spec for PeopleController

---
+++ spec/routing/socializer/people_routing_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+module Socializer
+  RSpec.describe PeopleController, type: :routing do
+    routes { Socializer::Engine.routes }
+
+    describe 'routing' do
+      it 'routes to #index' do
+        expect(:get => '/people').to route_to('socializer/people#index')
+      end
+
+      it 'does not route to #new' do
+        # expect(get: '/people/new').to_not be_routable
+        expect(get: '/people/new').to_not route_to('socializer/people#new')
+      end
+
+      it 'routes to #show' do
+        expect(:get => '/people/1').to route_to('socializer/people#show', :id => '1')
+      end
+
+      it 'routes to #edit' do
+        expect(:get => '/people/1/edit').to route_to('socializer/people#edit', :id => '1')
+      end
+
+      it 'does not route to #create' do
+        expect(post: '/people').to_not be_routable
+      end
+
+      it 'routes to #update' do
+        expect(:put => '/people/1').to route_to('socializer/people#update', :id => '1')
+      end
+
+      it 'does not route to #destroy' do
+        expect(delete: '/people/1').to_not be_routable
+      end
+    end
+  end
+end
