eb14f999ed7c57eec5905f71e7fe2277a20aa024
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-22 13:29:56 -0400

    add a TODO for the NotificationsController reset_unread_notifications after_action callback

--- app/controllers/socializer/notifications_controller.rb
+++ app/controllers/socializer/notifications_controller.rb
@@ -4,6 +4,7 @@
 module Socializer
   class NotificationsController < ApplicationController
     before_action :authenticate_user
+    # TODO: Does this need to be an after_action? Can reset_unread_notifications be called at the end of the index action?
     after_action :reset_unread_notifications, only: [:index]
 
     # GET /notifications
@@ -15,6 +16,7 @@ module Socializer
     def show
       notification = Notification.find_by(id: params[:id])
       notification.mark_as_read if notification.unread?
+
       redirect_to activity_activities_path(activity_id: notification.activity.id)
     end
 
