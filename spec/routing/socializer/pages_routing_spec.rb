a8a1de2861ec12fe360929b42ee766a9caa7b5f4
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-23 10:54:03 -0400

    routing spec for PagesController

---
+++ spec/routing/socializer/pages_routing_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+module Socializer
+  RSpec.describe PagesController, type: :routing do
+    routes { Socializer::Engine.routes }
+
+    describe 'routing' do
+      it 'routes to #index' do
+        expect(get: '/').to route_to('socializer/pages#index')
+      end
+
+      it 'does not route to #new' do
+        expect(get: '/pages/new').to_not be_routable
+      end
+
+      it 'does not route to #show' do
+        expect(get: '/pages/1').to_not be_routable
+      end
+
+      it 'does not route to #edit' do
+        expect(get: '/pages/1/edit').to_not be_routable
+      end
+
+      it 'does not route to #create' do
+        expect(post: '/pages').to_not be_routable
+      end
+
+      it 'does not route to #update' do
+        expect(patch: '/pages/1').to_not be_routable
+      end
+
+      it 'does not route to #destroy' do
+        expect(delete: '/pages/1').to_not be_routable
+      end
+    end
+  end
+end
