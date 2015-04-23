85fc63b340af3c9bea422fff4b694435843b14a7
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-23 11:42:09 -0400

    add tests for PagesController

---
+++ spec/controllers/socializer/pages_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+module Socializer
+  RSpec.describe PagesController, type: :controller do
+    routes { Socializer::Engine.routes }
+
+    describe "GET #index" do
+      before :each do
+        get :index
+      end
+
+      context 'when not logged in' do
+        it "returns http success" do
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'when logged in' do
+        it 'is a pending example'
+        # # Create a user
+        # let(:user) { create(:socializer_person) }
+
+        # # Setting the current user
+        # before { cookies[:user_id] = user.guid }
+        # before :each do
+        #   allow(controller).to receive(:signed_in?).and_return(true)
+        # end
+
+        # it 'redirects to activities#index' do
+        #   expect(response).to redirect_to activities_path
+        # end
+      end
+    end
+  end
+end
