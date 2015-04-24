51b6c1bcd8dbb0a9ac418c6860deb546c6f8ba44
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-24 12:15:53 -0400

    add the circle association to the tie factory

--- spec/factories/socializer_ties.rb
+++ spec/factories/socializer_ties.rb
@@ -2,6 +2,7 @@
 
 FactoryGirl.define do
   factory :socializer_tie, class: Socializer::Tie do
+    association :circle, factory: :socializer_circle
     association :activity_contact, factory: :socializer_activity_object_person
   end
 end
