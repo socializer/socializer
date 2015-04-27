6309a6f07b475573af77227d7e772fa9c0275a35
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-27 14:15:29 -0400

    routing spec for AuthenticationsController

---
+++ spec/routing/socializer/authentications_routing_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+module Socializer
+  RSpec.describe AuthenticationsController, type: :routing do
+    routes { Socializer::Engine.routes }
+
+    describe 'routing' do
+      it 'routes to #index' do
+        expect(get: '/authentications').to route_to('socializer/authentications#index')
+      end
+
+      it 'does not route to #new' do
+        expect(get: '/authentications/new').to_not be_routable
+      end
+
+      it 'does not route to #show' do
+        expect(get: '/authentications/1').to_not be_routable
+      end
+
+      it 'does not route to #edit' do
+        expect(get: '/authentications/1/edit').to_not be_routable
+      end
+
+      it 'does not route to #create' do
+        expect(post: '/authentications').to_not be_routable
+      end
+
+      it 'does not route to #update' do
+        expect(patch: '/authentications/1').to_not be_routable
+      end
+
+      it 'does not route to #destroy' do
+        expect(delete: '/authentications/1').to route_to('socializer/authentications#destroy', id: '1')
+      end
+    end
+  end
+end
