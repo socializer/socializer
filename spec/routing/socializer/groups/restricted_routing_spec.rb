d8484b91ad7b8068610210ad0b4fd4ee1525be3a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-05-01 11:37:27 -0400

    routing spec for Groups::RestrictedController

---
+++ spec/routing/socializer/groups/restricted_routing_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+module Socializer
+  RSpec.describe Groups::RestrictedController, type: :routing do
+    routes { Socializer::Engine.routes }
+
+    describe 'routing' do
+      it 'routes to #index' do
+        expect(get: '/groups/restricted').to route_to('socializer/groups/restricted#index')
+      end
+
+      it 'does not route to #new' do
+        expect(get: '/groups/restricted/new').to_not be_routable
+      end
+
+      it 'does not route to #show' do
+        expect(get: '/groups/restricted/show').to_not be_routable
+      end
+
+      it 'does not route to #edit' do
+        expect(get: '/groups/restricted/1/edit').to_not be_routable
+      end
+
+      it 'does not route to #create' do
+        expect(post: '/groups/restricted').to_not be_routable
+      end
+
+      it 'does not route to #update' do
+        expect(patch: '/groups/restricted/1').to_not be_routable
+        expect(put: '/groups/restricted/1').to_not be_routable
+      end
+
+      it 'does not route to #destroy' do
+        expect(delete: '/groups/restricted/1').to_not be_routable
+      end
+    end
+  end
+end
