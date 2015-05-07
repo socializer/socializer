84c57ef52484883b0a20ff026b426e8fd374a73f
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-05-07 13:04:36 -0400

    add tests for People::ContributionsController

---
+++ spec/controllers/socializer/people/contributions_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+module Socializer
+  RSpec.describe People::ContributionsController, type: :controller do
+    routes { Socializer::Engine.routes }
+
+    # Create a user nad contribution
+    let(:user) { create(:socializer_person) }
+    let(:valid_attributes) { { label: 'test', url: 'http://test.org' } }
+    let(:contribution) { user.contributions.create!(valid_attributes) }
+
+    # Setting the current user
+    before { cookies[:user_id] = user.guid }
+
+    it { should use_before_action(:authenticate_user) }
+
+    describe 'POST #create' do
+      context 'with valid attributes' do
+        it 'saves the new contribution in the database' do
+          expect { post :create, person_contribution: valid_attributes, person_id: user }.to change(PersonContribution, :count).by(1)
+        end
+
+        it 'redirects to people#show' do
+          post :create, person_contribution: valid_attributes, person_id: user
+          expect(response).to redirect_to user
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'is a pending example'
+      end
+    end
+
+    describe 'PATCH #update' do
+      context 'with valid attributes' do
+        it 'redirects to people#show' do
+          patch :update, id: contribution, person_id: user, person_contribution: { label: 'updated content' }
+          expect(response).to redirect_to user
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'is a pending example'
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'deletes the contribution' do
+        contribution
+        expect { delete :destroy, id: contribution, person_id: user }.to change(PersonContribution, :count).by(-1)
+      end
+
+      it 'redirects to people#show' do
+        delete :destroy, id: contribution, person_id: user
+        expect(response).to redirect_to user
+      end
+    end
+  end
+end
