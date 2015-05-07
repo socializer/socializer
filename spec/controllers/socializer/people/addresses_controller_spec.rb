9939b1a0320967bbb8a7a16567ef1e3198446096
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-05-07 13:14:48 -0400

    add tests for People::AddressesController

---
+++ spec/controllers/socializer/people/addresses_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+module Socializer
+  RSpec.describe People::AddressesController, type: :controller do
+    routes { Socializer::Engine.routes }
+
+    # Create a user nad address
+    let(:user) { create(:socializer_person) }
+    let(:valid_attributes) { { category: :home, postal_code_or_zip: '12345' } }
+    let(:address) { user.addresses.create!(valid_attributes) }
+
+    # Setting the current user
+    before { cookies[:user_id] = user.guid }
+
+    it { should use_before_action(:authenticate_user) }
+
+    describe 'POST #create' do
+      context 'with valid attributes' do
+        it 'saves the new address in the database' do
+          expect { post :create, person_address: valid_attributes, person_id: user }.to change(PersonAddress, :count).by(1)
+        end
+
+        it 'redirects to people#show' do
+          post :create, person_address: valid_attributes, person_id: user
+          expect(response).to redirect_to user
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'is a pending example'
+      end
+    end
+
+    describe 'PATCH #update' do
+      context 'with valid attributes' do
+        it 'redirects to people#show' do
+          patch :update, id: address, person_id: user, person_address: { label: 'updated content' }
+          expect(response).to redirect_to user
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'is a pending example'
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'deletes the address' do
+        address
+        expect { delete :destroy, id: address, person_id: user }.to change(PersonAddress, :count).by(-1)
+      end
+
+      it 'redirects to people#show' do
+        delete :destroy, id: address, person_id: user
+        expect(response).to redirect_to user
+      end
+    end
+  end
+end
