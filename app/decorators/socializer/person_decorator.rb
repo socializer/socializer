cc7c9e73ea2cb7d22343e51d24c12d7c446dfd5f
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-09 12:12:40 -0400

    simplify the toolbar_link method

--- app/decorators/socializer/person_decorator.rb
+++ app/decorators/socializer/person_decorator.rb
@@ -100,17 +100,36 @@ module Socializer
       end
     end
 
+    # Check if the url is the current page
+    #
+    # @param url: [String] The url to check
+    #
+    # @return [String/Nil] Returns 'active' if the url is the current page.
+    def toolbar_item_class(url:)
+      'active' if helpers.current_page?(url)
+    end
+
     def toolbar_links(list)
       list.map do |item|
         toolbar_link(item)
       end.join.html_safe
     end
 
+    # Check if the object is an instance of {Socializer::Membership}
+    #
+    # @param object:
+    #
+    # @return [Socializer::Group]
+    def toolbar_object(object:)
+      return object.group if object.is_a?(Socializer::Membership)
+      object
+    end
+
     def toolbar_link(item)
-      item       = item.group if item.is_a?(Socializer::Membership)
+      item       = toolbar_object(object: item)
       url_prefix = item.class.name.demodulize.downcase
       url        = helpers.public_send("#{url_prefix}_activities_path", item.id)
-      class_name = 'active' if helpers.current_page?(url)
+      class_name = toolbar_item_class(url: url)
 
       helpers.content_tag(:li) do
         helpers.link_to(item.display_name, url, class: class_name)
