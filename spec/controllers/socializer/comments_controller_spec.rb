42ed1a5e03f493cfd08f2a27a1d5e20a7a8920d9
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-19 11:39:21 -0400

    CommentsController: begin testing when not logged in

--- spec/controllers/socializer/comments_controller_spec.rb
+++ spec/controllers/socializer/comments_controller_spec.rb
@@ -8,79 +8,119 @@ module Socializer
     let(:user) { create(:socializer_person) }
     let(:comment) { create(:socializer_comment, activity_author: user.activity_object) }
     let(:note) { create(:socializer_note) }
+    let(:valid_attributes) { { comment: { content: 'This is a comment', activity_target_id: note.guid } } }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #new' do
-      before :each do
-        get :new
+    describe 'when not logged in' do
+      describe 'GET #new' do
+        it 'requires login' do
+          get :new
+          expect(response).to redirect_to root_path
+        end
       end
 
-      it 'assigns a new Comment to @model' do
-        expect(assigns(:comment)).to be_a_new(Comment)
+      describe 'POST #create' do
+        it 'requires login' do
+          post :create, valid_attributes
+          expect(response).to redirect_to root_path
+        end
       end
 
-      it 'renders the :new template' do
-        expect(response).to render_template :new
+      describe 'GET #edit' do
+        it 'requires login' do
+          get :edit, id: comment
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      describe 'PATCH #update' do
+        it 'requires login' do
+          patch :update, id: comment, comment: { content: 'This is a comment update' }
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      describe 'DELETE #destroy' do
+        it 'requires login' do
+          delete :destroy, id: comment
+          expect(response).to redirect_to root_path
+        end
       end
     end
 
-    describe 'POST #create' do
-      context 'with valid attributes' do
-        it 'saves the new comment in the database' do
-          expect { post :create, comment: { content: 'This is a comment' } }.to change(Comment, :count).by(1)
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
+
+      it { should use_before_action(:authenticate_user) }
+
+      describe 'GET #new' do
+        before :each do
+          get :new
+        end
+
+        it 'assigns a new Comment to @model' do
+          expect(assigns(:comment)).to be_a_new(Comment)
+        end
+
+        it 'renders the :new template' do
+          expect(response).to render_template :new
+        end
+      end
+
+      describe 'POST #create' do
+        context 'with valid attributes' do
+          it 'saves the new comment in the database' do
+            expect { post :create, valid_attributes }.to change(Comment, :count).by(1)
+          end
+
+          it 'redirects to activities#index' do
+            post :create, valid_attributes
+            expect(response).to redirect_to activities_path
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'is a pending example'
+        end
+      end
+
+      describe 'GET #edit' do
+        before :each do
+          get :edit, id: comment
+        end
+
+        it 'assigns the requested comment to @comment' do
+          expect(assigns(:comment)).to eq comment
+        end
+
+        it 'renders the :edit template' do
+          expect(response).to render_template :edit
+        end
+      end
+
+      describe 'PATCH #update' do
+        context 'with valid attributes' do
+          it 'redirects to activities#index' do
+            patch :update, id: comment, comment: { content: 'This is a comment' }
+            expect(response).to redirect_to activities_path
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'is a pending example'
+        end
+      end
+
+      describe 'DELETE #destroy' do
+        it 'deletes the comment' do
+          comment
+          expect { delete :destroy, id: comment }.to change(Comment, :count).by(-1)
         end
 
         it 'redirects to activities#index' do
-          post :create, comment: { content: 'This is a comment', activity_target_id: note.guid }
+          delete :destroy, id: comment
           expect(response).to redirect_to activities_path
         end
-      end
-
-      context 'with invalid attributes' do
-        it 'is a pending example'
-      end
-    end
-
-    describe 'GET #edit' do
-      before :each do
-        get :edit, id: comment
-      end
-
-      it 'assigns the requested comment to @comment' do
-        expect(assigns(:comment)).to eq comment
-      end
-
-      it 'renders the :edit template' do
-        expect(response).to render_template :edit
-      end
-    end
-
-    describe 'PATCH #update' do
-      context 'with valid attributes' do
-        it 'redirects to activities#index' do
-          patch :update, id: comment, comment: { content: 'This is a comment' }
-          expect(response).to redirect_to activities_path
-        end
-      end
-
-      context 'with invalid attributes' do
-        it 'is a pending example'
-      end
-    end
-
-    describe 'DELETE #destroy' do
-      it 'deletes the comment' do
-        comment
-        expect { delete :destroy, id: comment }.to change(Comment, :count).by(-1)
-      end
-
-      it 'redirects to activities#index' do
-        delete :destroy, id: comment
-        expect(response).to redirect_to activities_path
       end
     end
   end
