1fef665dce6f978a8ed165fcc48d01536c712390
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-19 11:06:39 -0400

    ActivitiesController: begin testing when not logged in

--- spec/controllers/socializer/activities_controller_spec.rb
+++ spec/controllers/socializer/activities_controller_spec.rb
@@ -10,53 +10,51 @@ module Socializer
     let(:result) { ActivityCreator.new(actor_id: user.guid, activity_object_id: note.guid, verb: 'post', object_ids: 'public').perform }
     let(:activity) { result.activity.decorate }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #index' do
-      before :each do
-        get :index
+    describe 'when not logged in' do
+      describe 'GET #index' do
+        it 'requires login' do
+          get :index
+          expect(response).to redirect_to root_path
+        end
       end
 
-      it 'assigns @activities' do
-        expect(assigns(:activities)).to match_array([activity])
-      end
-
-      it 'assigns @current_id' do
-        expect(assigns(:current_id)).to eq(nil)
-      end
-
-      it 'assigns @title' do
-        expect(assigns(:title)).to match('Activity stream')
-      end
-
-      it 'assigns @note' do
-        expect(assigns(:note)).to be_a_new(Note)
-      end
-
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
-
-      it 'returns http success' do
-        expect(response).to have_http_status(:success)
+      describe 'DELETE #destroy' do
+        it 'requires login' do
+          delete :destroy, id: activity, format: :js
+          expect(response).to redirect_to root_path
+        end
       end
     end
 
-    describe 'DELETE #destroy' do
-      context 'assigns variables and returns success' do
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
+
+      it { should use_before_action(:authenticate_user) }
+
+      describe 'GET #index' do
         before :each do
-          delete :destroy,  id: activity, format: :js
+          get :index
         end
 
-        it 'assigns @activity' do
-          expect(assigns(:activity)).to eq(activity)
+        it 'assigns @activities' do
+          expect(assigns(:activities)).to match_array([activity])
         end
 
-        it 'assigns @activity_guid' do
-          expect(assigns(:activity_guid)).to eq(activity.guid)
+        it 'assigns @current_id' do
+          expect(assigns(:current_id)).to eq(nil)
+        end
+
+        it 'assigns @title' do
+          expect(assigns(:title)).to match('Activity stream')
+        end
+
+        it 'assigns @note' do
+          expect(assigns(:note)).to be_a_new(Note)
+        end
+
+        it 'renders the :index template' do
+          expect(response).to render_template :index
         end
 
         it 'returns http success' do
@@ -64,9 +62,29 @@ module Socializer
         end
       end
 
-      it 'deletes the activity' do
-        activity
-        expect { delete :destroy, id: activity, format: :js }.to change(Activity, :count).by(-1)
+      describe 'DELETE #destroy' do
+        context 'assigns variables and returns success' do
+          before :each do
+            delete :destroy,  id: activity, format: :js
+          end
+
+          it 'assigns @activity' do
+            expect(assigns(:activity)).to eq(activity)
+          end
+
+          it 'assigns @activity_guid' do
+            expect(assigns(:activity_guid)).to eq(activity.guid)
+          end
+
+          it 'returns http success' do
+            expect(response).to have_http_status(:success)
+          end
+        end
+
+        it 'deletes the activity' do
+          activity
+          expect { delete :destroy, id: activity, format: :js }.to change(Activity, :count).by(-1)
+        end
       end
     end
   end
