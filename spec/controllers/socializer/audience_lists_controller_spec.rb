8f75236afdb9bf5c2a8aabd9f716da4335d5fa80
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-19 11:12:23 -0400

    AudienceListsController: begin testing when not logged in

--- spec/controllers/socializer/audience_lists_controller_spec.rb
+++ spec/controllers/socializer/audience_lists_controller_spec.rb
@@ -7,36 +7,47 @@ module Socializer
     # Create a user
     let(:user) { create(:socializer_person) }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #index' do
-      before :each do
-        get :index, format: :json
-      end
-
-      context 'returns default values' do
-        let(:json) { JSON.parse(response.body) }
-
-        it { expect(json.count).to eq(2) }
-
-        it 'public' do
-          expect(json.first['id']).to match('public')
-          expect(json.first['name']).to match('Public')
-          expect(json.first['icon']).to match('fa-globe')
-        end
-
-        it 'circles' do
-          expect(json.last['id']).to match('circles')
-          expect(json.last['name']).to match('Circles')
-          expect(json.last['icon']).to match('fa-google-circles')
+    describe 'when not logged in' do
+      describe 'GET #index' do
+        it 'requires login' do
+          get :index, format: :json
+          expect(response).to redirect_to root_path
         end
       end
+    end
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:success)
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
+
+      it { should use_before_action(:authenticate_user) }
+
+      describe 'GET #index' do
+        before :each do
+          get :index, format: :json
+        end
+
+        context 'returns default values' do
+          let(:json) { JSON.parse(response.body) }
+
+          it { expect(json.count).to eq(2) }
+
+          it 'public' do
+            expect(json.first['id']).to match('public')
+            expect(json.first['name']).to match('Public')
+            expect(json.first['icon']).to match('fa-globe')
+          end
+
+          it 'circles' do
+            expect(json.last['id']).to match('circles')
+            expect(json.last['name']).to match('Circles')
+            expect(json.last['icon']).to match('fa-google-circles')
+          end
+        end
+
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
       end
     end
   end
