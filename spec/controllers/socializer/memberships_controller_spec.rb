0a08c8bfc1e7c184622feb6a43367474863d26b9
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-19 11:58:08 -0400

    MembershipsController: add/use valid_attributes

--- spec/controllers/socializer/memberships_controller_spec.rb
+++ spec/controllers/socializer/memberships_controller_spec.rb
@@ -8,6 +8,7 @@ module Socializer
     let(:user) { create(:socializer_person) }
     let(:group) { create(:socializer_group, activity_author: user.activity_object) }
     let(:membership) { create(:socializer_membership, group: group, activity_member: user.activity_object) }
+    let(:valid_attributes) { { membership: { group_id: group.id } } }
 
     # Setting the current user
     before { cookies[:user_id] = user.guid }
@@ -18,11 +19,11 @@ module Socializer
       context 'with valid attributes' do
         it 'saves the new membership in the database' do
           group
-          expect { post :create, membership: { group_id: group.id } }.to change(Membership, :count).by(1)
+          expect { post :create, valid_attributes }.to change(Membership, :count).by(1)
         end
 
         it 'redirects to groups#show' do
-          post :create, membership: { group_id: group.id }
+          post :create, valid_attributes
           expect(response).to redirect_to group
         end
       end
