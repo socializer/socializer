921b23782e74149d470258323efdd755c87bf757
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-19 18:58:54 -0400

    TiesController: begin testing when not logged in

--- spec/controllers/socializer/ties_controller_spec.rb
+++ spec/controllers/socializer/ties_controller_spec.rb
@@ -10,53 +10,71 @@ module Socializer
     let(:tie) { create(:socializer_tie, activity_contact: user.activity_object, circle: circle) }
     let(:valid_attributes) { { tie: { circle_id: circle.id, contact_id: user.activity_object.id } } }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'POST #create' do
-      before :each do
-        @request.env['HTTP_ACCEPT'] = 'application/javascript'
-      end
-
-      context 'with valid attributes' do
-        it 'saves the new group in the database' do
-          expect { post :create, valid_attributes }.to change(Tie, :count).by(1)
-        end
-
-        it 'returns http ok' do
-          post :create, valid_attributes
-          expect(response).to have_http_status(:ok)
+    describe 'when not logged in' do
+      describe 'POST #create' do
+        it 'requires login' do
+          post :create, valid_attributes, format: :js
+          expect(response).to redirect_to root_path
         end
       end
 
-      context 'with invalid attributes' do
-        it 'is a pending example'
+      describe 'DELETE #destroy' do
+        it 'requires login' do
+          delete :destroy, id: tie
+          expect(response).to redirect_to root_path
+        end
       end
     end
 
-    describe 'DELETE #destroy' do
-      it 'deletes the tie' do
-        tie
-        expect { delete :destroy, id: tie }.to change(Tie, :count).by(-1)
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
+
+      it { should use_before_action(:authenticate_user) }
+
+      describe 'POST #create' do
+        before :each do
+          @request.env['HTTP_ACCEPT'] = 'application/javascript'
+        end
+
+        context 'with valid attributes' do
+          it 'saves the new group in the database' do
+            expect { post :create, valid_attributes }.to change(Tie, :count).by(1)
+          end
+
+          it 'returns http ok' do
+            post :create, valid_attributes
+            expect(response).to have_http_status(:ok)
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'is a pending example'
+        end
       end
 
-      context 'check the variables and redirect' do
-        before :each do
-          delete :destroy, id: tie
+      describe 'DELETE #destroy' do
+        it 'deletes the tie' do
+          tie
+          expect { delete :destroy, id: tie }.to change(Tie, :count).by(-1)
         end
 
-        it 'assigns @tie' do
-          expect(assigns(:tie)).to eq tie
-        end
+        context 'check the variables and redirect' do
+          before :each do
+            delete :destroy, id: tie
+          end
 
-        it 'assigns @circle' do
-          expect(assigns(:circle)).to eq circle
-        end
+          it 'assigns @tie' do
+            expect(assigns(:tie)).to eq tie
+          end
 
-        it 'redirects to circles#show' do
-          expect(response).to redirect_to circle
+          it 'assigns @circle' do
+            expect(assigns(:circle)).to eq circle
+          end
+
+          it 'redirects to circles#show' do
+            expect(response).to redirect_to circle
+          end
         end
       end
     end
