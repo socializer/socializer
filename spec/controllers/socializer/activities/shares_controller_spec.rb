70d74c4e170f2638b6cd655d4c4df84c02f35c98
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-22 12:02:54 -0400

    Activities::SharesController: begin testing when not logged in

--- spec/controllers/socializer/activities/shares_controller_spec.rb
+++ spec/controllers/socializer/activities/shares_controller_spec.rb
@@ -8,45 +8,62 @@ module Socializer
       # Create a user and a note to share
       let(:user) { create(:socializer_person) }
       let(:note) { create(:socializer_note) }
+      let(:object_ids) { Socializer::Audience.privacy.find_value(:public).value.split(',') }
 
-      # Setting the current user
-      before { cookies[:user_id] = user.guid }
-
-      it { should use_before_action(:authenticate_user) }
-
-      describe 'GET #new' do
-        # Visit the new page
-        before { get :new, id: note.guid }
-
-        it 'return an activity object' do
-          expect(assigns(:activity_object)).to eq(note.activity_object)
-        end
-
-        it 'return an share object' do
-          expect(assigns(:share)).to eq(note)
-        end
-
-        it 'renders the :new template' do
-          expect(response).to render_template :new
-        end
-      end
-
-      describe 'POST #create' do
-        context 'with valid attributes' do
-          let(:object_ids) { Socializer::Audience.privacy.find_value(:public).value.split(',') }
-
-          it 'redirects to circles#contacts' do
-            post :create, share: { content: '', object_ids: object_ids, activity_id: note.guid }, id: note
-            expect(response).to redirect_to activities_path
+      describe 'when not logged in' do
+        describe 'GET #new' do
+          it 'requires login' do
+            get :new, id: note.guid
+            expect(response).to redirect_to root_path
           end
         end
 
-        context 'with invalid attributes' do
-          it 'is a pending example'
-          # it 're-renders the :new template' do
-          #   post :create, invalid_attributes
-          #   expect(response).to render_template :new
-          # end
+        describe 'POST #create' do
+          it 'requires login' do
+            post :create, share: { content: '', object_ids: object_ids, activity_id: note.guid }, id: note
+            expect(response).to redirect_to root_path
+          end
+        end
+      end
+
+      describe 'when logged in' do
+        # Setting the current user
+        before { cookies[:user_id] = user.guid }
+
+        it { should use_before_action(:authenticate_user) }
+
+        describe 'GET #new' do
+          # Visit the new page
+          before { get :new, id: note.guid }
+
+          it 'return an activity object' do
+            expect(assigns(:activity_object)).to eq(note.activity_object)
+          end
+
+          it 'return an share object' do
+            expect(assigns(:share)).to eq(note)
+          end
+
+          it 'renders the :new template' do
+            expect(response).to render_template :new
+          end
+        end
+
+        describe 'POST #create' do
+          context 'with valid attributes' do
+            it 'redirects to circles#contacts' do
+              post :create, share: { content: '', object_ids: object_ids, activity_id: note.guid }, id: note
+              expect(response).to redirect_to activities_path
+            end
+          end
+
+          context 'with invalid attributes' do
+            it 'is a pending example'
+            # it 're-renders the :new template' do
+            #   post :create, invalid_attributes
+            #   expect(response).to render_template :new
+            # end
+          end
         end
       end
     end
