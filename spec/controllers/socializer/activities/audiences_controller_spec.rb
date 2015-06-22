cff01757e699177fabb63c4dcb00e094a183432e
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-22 11:46:52 -0400

    Activities::AudiencesController: begin testing when not logged in

--- spec/controllers/socializer/activities/audiences_controller_spec.rb
+++ spec/controllers/socializer/activities/audiences_controller_spec.rb
@@ -10,26 +10,37 @@ module Socializer
     let(:activity) { Activity.find_by(activity_object_id: note.activity_object.id) }
     let(:audience_list) { ActivityAudienceList.new(activity: activity).perform }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #index' do
-      before :each do
-        get :index, id: activity
+    describe 'when not logged in' do
+      describe 'GET #index' do
+        it 'requires login' do
+          get :index, id: activity
+          expect(response).to redirect_to root_path
+        end
       end
+    end
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:success)
-      end
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
 
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
+      it { should use_before_action(:authenticate_user) }
 
-      it 'assigns @object_ids' do
-        expect(assigns(:object_ids)).to match(audience_list)
+      describe 'GET #index' do
+        before :each do
+          get :index, id: activity
+        end
+
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
+
+        it 'renders the :index template' do
+          expect(response).to render_template :index
+        end
+
+        it 'assigns @object_ids' do
+          expect(assigns(:object_ids)).to match(audience_list)
+        end
       end
     end
   end
