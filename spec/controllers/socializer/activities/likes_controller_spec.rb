733e3ed9c50fecdefff4b233fd130b2ddf77fc9c
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-22 11:54:49 -0400

    Activities::LikesController: begin testing when not logged in

--- spec/controllers/socializer/activities/likes_controller_spec.rb
+++ spec/controllers/socializer/activities/likes_controller_spec.rb
@@ -9,63 +9,88 @@ module Socializer
       let(:user) { create(:socializer_person) }
       let(:note_activity) { create(:socializer_activity) }
 
-      # Setting the current user
-      before { cookies[:user_id] = user.guid }
-
-      it { should use_before_action(:authenticate_user) }
-
-      describe 'Set likable and activity' do
-        # Verify that the likable variable is set before create and destroy action
-        [:create, :destroy].each do |action|
-          before { post action, id: note_activity.guid, format: :js }
-
-          it "set likable for action #{action}" do
-            expect(assigns(:likable)).to eq(note_activity.activity_object)
+      describe 'when not logged in' do
+        describe 'GET #index' do
+          it 'requires login' do
+            get :index,  id: note_activity.id, format: :html
+            expect(response).to redirect_to root_path
           end
+        end
 
-          it "set activity for action #{action}" do
-            expect(assigns(:activity)).to eq(note_activity)
+        describe 'POST #create' do
+          it 'requires login' do
+            post :create, id: note_activity.guid, format: :js
+            expect(response).to redirect_to root_path
+          end
+        end
+
+        describe 'DELETE #destroy' do
+          it 'requires login' do
+            delete :destroy, id: note_activity.guid, format: :js
+            expect(response).to redirect_to root_path
           end
         end
       end
 
-      # Make sure that the note is not liked before liking it.
-      it 'no likes for the note before liking it' do
-        expect(user.likes?(note_activity.activity_object)).to be_falsey
-      end
+      describe 'when logged in' do
+        # Setting the current user
+        before { cookies[:user_id] = user.guid }
 
-      describe 'GET #create' do
-        # Create a like
-        before { post :create, id: note_activity.guid, format: :js }
+        it { should use_before_action(:authenticate_user) }
 
-        it 'likes the note after liking it' do
-          expect(user.likes?(note_activity.activity_object)).to be_truthy
+        describe 'Set likable and activity' do
+          # Verify that the likable variable is set before create and destroy action
+          [:create, :destroy].each do |action|
+            before { post action, id: note_activity.guid, format: :js }
+
+            it "set likable for action #{action}" do
+              expect(assigns(:likable)).to eq(note_activity.activity_object)
+            end
+
+            it "set activity for action #{action}" do
+              expect(assigns(:activity)).to eq(note_activity)
+            end
+          end
         end
-      end
 
-      describe 'GET #destroy' do
-        # Create a like
-        before { post :create,  id: note_activity.guid, format: :js }
-        # Destroy the like
-        before { post :destroy, id: note_activity.guid, format: :js }
-
-        it 'does not like the note anymore' do
+        # Make sure that the note is not liked before liking it.
+        it 'no likes for the note before liking it' do
           expect(user.likes?(note_activity.activity_object)).to be_falsey
         end
-      end
 
-      describe 'GET #index' do
-        # Create a like
-        before { post :create, id: note_activity.guid, format: :js }
-        # Get the people ou like the activity
-        before { get :index,  id: note_activity.id, format: :html }
+        describe 'GET #create' do
+          # Create a like
+          before { post :create, id: note_activity.guid, format: :js }
 
-        it 'return people' do
-          expect(assigns(:people)).to be_present
+          it 'likes the note after liking it' do
+            expect(user.likes?(note_activity.activity_object)).to be_truthy
+          end
         end
 
-        it 'return the user who like the activity' do
-          expect(assigns(:people)).to include(user)
+        describe 'GET #destroy' do
+          # Create a like
+          before { post :create,  id: note_activity.guid, format: :js }
+          # Destroy the like
+          before { post :destroy, id: note_activity.guid, format: :js }
+
+          it 'does not like the note anymore' do
+            expect(user.likes?(note_activity.activity_object)).to be_falsey
+          end
+        end
+
+        describe 'GET #index' do
+          # Create a like
+          before { post :create, id: note_activity.guid, format: :js }
+          # Get the people ou like the activity
+          before { get :index,  id: note_activity.id, format: :html }
+
+          it 'return people' do
+            expect(assigns(:people)).to be_present
+          end
+
+          it 'return the user who like the activity' do
+            expect(assigns(:people)).to include(user)
+          end
         end
       end
     end
