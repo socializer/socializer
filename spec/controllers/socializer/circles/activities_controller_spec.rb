3f4ea02fd355720be15fa834468a4cc7aa6f5062
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-23 12:15:20 -0400

    Circles::ActivitiesController: begin testing when not logged in

--- spec/controllers/socializer/circles/activities_controller_spec.rb
+++ spec/controllers/socializer/circles/activities_controller_spec.rb
@@ -9,38 +9,49 @@ module Socializer
     let(:circle) { create(:socializer_circle, activity_author: user.activity_object) }
     let(:activities) { Activity.circle_stream(actor_uid: circle.id, viewer_id: user.id).decorate }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #index' do
-      before :each do
-        get :index, circle_id: circle
+    describe 'when not logged in' do
+      describe 'GET #index' do
+        it 'requires login' do
+          get :index, circle_id: circle
+          expect(response).to redirect_to root_path
+        end
       end
+    end
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:success)
-      end
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
 
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
+      it { should use_before_action(:authenticate_user) }
 
-      it 'assigns @circle' do
-        expect(assigns(:circle)).to match(circle)
-      end
+      describe 'GET #index' do
+        before :each do
+          get :index, circle_id: circle
+        end
 
-      it 'assigns @title' do
-        expect(assigns(:title)).to match(circle.display_name)
-      end
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
 
-      it 'assigns @current_id' do
-        expect(assigns(:current_id)).to eq(circle.guid)
-      end
+        it 'renders the :index template' do
+          expect(response).to render_template :index
+        end
 
-      it 'assigns @activities' do
-        expect(assigns(:activities)).to match_array(activities)
+        it 'assigns @circle' do
+          expect(assigns(:circle)).to match(circle)
+        end
+
+        it 'assigns @title' do
+          expect(assigns(:title)).to match(circle.display_name)
+        end
+
+        it 'assigns @current_id' do
+          expect(assigns(:current_id)).to eq(circle.guid)
+        end
+
+        it 'assigns @activities' do
+          expect(assigns(:activities)).to match_array(activities)
+        end
       end
     end
   end
