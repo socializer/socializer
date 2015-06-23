624939c7d8d7f1384f95c7c049f652ae39a4f0a5
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-23 12:23:14 -0400

    Groups::ActivitiesController: begin testing when not logged in

--- spec/controllers/socializer/groups/activities_controller_spec.rb
+++ spec/controllers/socializer/groups/activities_controller_spec.rb
@@ -9,38 +9,49 @@ module Socializer
     let(:group) { create(:socializer_group, activity_author: user.activity_object) }
     let(:activities) { Activity.group_stream(actor_uid: group.id, viewer_id: user.id).decorate }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #index' do
-      before :each do
-        get :index, group_id: group
+    describe 'when not logged in' do
+      describe 'GET #index' do
+        it 'requires login' do
+          get :index, group_id: group
+          expect(response).to redirect_to root_path
+        end
       end
+    end
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:success)
-      end
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
 
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
+      it { should use_before_action(:authenticate_user) }
 
-      it 'assigns @group' do
-        expect(assigns(:group)).to match(group)
-      end
+      describe 'GET #index' do
+        before :each do
+          get :index, group_id: group
+        end
 
-      it 'assigns @title' do
-        expect(assigns(:title)).to match(group.display_name)
-      end
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
 
-      it 'assigns @current_id' do
-        expect(assigns(:current_id)).to eq(group.guid)
-      end
+        it 'renders the :index template' do
+          expect(response).to render_template :index
+        end
 
-      it 'assigns @activities' do
-        expect(assigns(:activities)).to match_array(activities)
+        it 'assigns @group' do
+          expect(assigns(:group)).to match(group)
+        end
+
+        it 'assigns @title' do
+          expect(assigns(:title)).to match(group.display_name)
+        end
+
+        it 'assigns @current_id' do
+          expect(assigns(:current_id)).to eq(group.guid)
+        end
+
+        it 'assigns @activities' do
+          expect(assigns(:activities)).to match_array(activities)
+        end
       end
     end
   end
