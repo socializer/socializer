da86501e058460c60775d16a5eaa24ff72d6eebb
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-23 12:42:47 -0400

    Groups::PendingInvitesController: begin testing when not logged in

--- spec/controllers/socializer/groups/pending_invites_controller_spec.rb
+++ spec/controllers/socializer/groups/pending_invites_controller_spec.rb
@@ -8,30 +8,41 @@ module Socializer
     let(:user) { create(:socializer_person) }
     let(:group) { create(:socializer_group, activity_author: user.activity_object) }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #index' do
-      before :each do
-        get :index
+    describe 'when not logged in' do
+      describe 'GET #index' do
+        it 'requires login' do
+          get :index
+          expect(response).to redirect_to root_path
+        end
       end
+    end
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:success)
-      end
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
 
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
+      it { should use_before_action(:authenticate_user) }
 
-      it 'assigns @pending_invites' do
-        expect(assigns(:pending_invites)).to match_array([])
-      end
+      describe 'GET #index' do
+        before :each do
+          get :index
+        end
 
-      context 'with a pending invite' do
-        it 'is a pending example'
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
+
+        it 'renders the :index template' do
+          expect(response).to render_template :index
+        end
+
+        it 'assigns @pending_invites' do
+          expect(assigns(:pending_invites)).to match_array([])
+        end
+
+        context 'with a pending invite' do
+          it 'is a pending example'
+        end
       end
     end
   end
