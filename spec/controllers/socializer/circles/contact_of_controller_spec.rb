0c007dbee48468d1fc933a36617636345911c0f9
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-23 12:16:56 -0400

    Circles::ContactOfController: begin testing when not logged in

--- spec/controllers/socializer/circles/contact_of_controller_spec.rb
+++ spec/controllers/socializer/circles/contact_of_controller_spec.rb
@@ -8,26 +8,37 @@ module Socializer
     let(:user) { create(:socializer_person) }
     let(:contact_of) { user.contact_of }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'GET #index' do
-      before :each do
-        get :index
+    describe 'when not logged in' do
+      describe 'GET #index' do
+        it 'requires login' do
+          get :index
+          expect(response).to redirect_to root_path
+        end
       end
+    end
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:success)
-      end
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
 
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
+      it { should use_before_action(:authenticate_user) }
 
-      it 'assigns @contact_of' do
-        expect(assigns(:contact_of)).to match_array(contact_of)
+      describe 'GET #index' do
+        before :each do
+          get :index
+        end
+
+        it 'returns http success' do
+          expect(response).to have_http_status(:success)
+        end
+
+        it 'renders the :index template' do
+          expect(response).to render_template :index
+        end
+
+        it 'assigns @contact_of' do
+          expect(assigns(:contact_of)).to match_array(contact_of)
+        end
       end
     end
   end
