14d49decd797e651516d10541c3793f10c5657d1
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-24 11:19:01 -0400

    People::EmploymentsController: begin testing when not logged in

--- spec/controllers/socializer/people/employments_controller_spec.rb
+++ spec/controllers/socializer/people/employments_controller_spec.rb
@@ -9,50 +9,75 @@ module Socializer
     let(:valid_attributes) { { employer_name: 'Test Company', job_title: 'Tester' } }
     let(:employment) { user.employments.create!(valid_attributes) }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'POST #create' do
-      context 'with valid attributes' do
-        it 'saves the new employment in the database' do
-          expect { post :create, person_employment: valid_attributes, person_id: user }.to change(PersonEmployment, :count).by(1)
-        end
-
-        it 'redirects to people#show' do
+    describe 'when not logged in' do
+      describe 'POST #create' do
+        it 'requires login' do
           post :create, person_employment: valid_attributes, person_id: user
-          expect(response).to redirect_to user
+          expect(response).to redirect_to root_path
         end
       end
 
-      context 'with invalid attributes' do
-        it 'is a pending example'
-      end
-    end
-
-    describe 'PATCH #update' do
-      context 'with valid attributes' do
-        it 'redirects to people#show' do
+      describe 'PATCH #update' do
+        it 'requires login' do
           patch :update, id: employment, person_id: user, person_employment: { label: 'updated content' }
-          expect(response).to redirect_to user
+          expect(response).to redirect_to root_path
         end
       end
 
-      context 'with invalid attributes' do
-        it 'is a pending example'
+      describe 'DELETE #destroy' do
+        it 'requires login' do
+          delete :destroy, id: employment, person_id: user
+          expect(response).to redirect_to root_path
+        end
       end
     end
 
-    describe 'DELETE #destroy' do
-      it 'deletes the employment' do
-        employment
-        expect { delete :destroy, id: employment, person_id: user }.to change(PersonEmployment, :count).by(-1)
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
+
+      it { should use_before_action(:authenticate_user) }
+
+      describe 'POST #create' do
+        context 'with valid attributes' do
+          it 'saves the new employment in the database' do
+            expect { post :create, person_employment: valid_attributes, person_id: user }.to change(PersonEmployment, :count).by(1)
+          end
+
+          it 'redirects to people#show' do
+            post :create, person_employment: valid_attributes, person_id: user
+            expect(response).to redirect_to user
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'is a pending example'
+        end
       end
 
-      it 'redirects to people#show' do
-        delete :destroy, id: employment, person_id: user
-        expect(response).to redirect_to user
+      describe 'PATCH #update' do
+        context 'with valid attributes' do
+          it 'redirects to people#show' do
+            patch :update, id: employment, person_id: user, person_employment: { label: 'updated content' }
+            expect(response).to redirect_to user
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'is a pending example'
+        end
+      end
+
+      describe 'DELETE #destroy' do
+        it 'deletes the employment' do
+          employment
+          expect { delete :destroy, id: employment, person_id: user }.to change(PersonEmployment, :count).by(-1)
+        end
+
+        it 'redirects to people#show' do
+          delete :destroy, id: employment, person_id: user
+          expect(response).to redirect_to user
+        end
       end
     end
   end
