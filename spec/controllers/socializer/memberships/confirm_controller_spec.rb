24ac4de5e129022ef43ba87800662c36a629a785
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-24 10:34:36 -0400

    Memberships::ConfirmController: begin testing when not logged in

--- spec/controllers/socializer/memberships/confirm_controller_spec.rb
+++ spec/controllers/socializer/memberships/confirm_controller_spec.rb
@@ -9,30 +9,41 @@ module Socializer
     let(:group) { create(:socializer_group, activity_author: user.activity_object) }
     let(:membership) { create(:socializer_membership, active: false, group: group, activity_member: user.activity_object) }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'POST #create' do
-      context 'with valid attributes' do
-        it 'confirm the membership' do
-          expect(membership.active).to be false
-
+    describe 'when not logged in' do
+      describe 'POST #create' do
+        it 'requires login' do
           post :create, id: membership.id
-
-          expect(membership.reload.active).to eq(true)
-          expect(assigns(:membership)).to eq membership
-        end
-
-        it 'redirects to groups#show' do
-          post :create, id: membership.id
-          expect(response).to redirect_to membership.group
+          expect(response).to redirect_to root_path
         end
       end
+    end
 
-      context 'with invalid attributes' do
-        it 'is a pending example'
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
+
+      it { should use_before_action(:authenticate_user) }
+
+      describe 'POST #create' do
+        context 'with valid attributes' do
+          it 'confirm the membership' do
+            expect(membership.active).to be false
+
+            post :create, id: membership.id
+
+            expect(membership.reload.active).to eq(true)
+            expect(assigns(:membership)).to eq membership
+          end
+
+          it 'redirects to groups#show' do
+            post :create, id: membership.id
+            expect(response).to redirect_to membership.group
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'is a pending example'
+        end
       end
     end
   end
