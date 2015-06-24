41740eb66ca9fa40d110f3791f5594050d88f597
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-06-24 10:38:37 -0400

    Memberships::DeclineController: begin testing when not logged in

--- spec/controllers/socializer/memberships/decline_controller_spec.rb
+++ spec/controllers/socializer/memberships/decline_controller_spec.rb
@@ -9,26 +9,37 @@ module Socializer
     let(:group) { create(:socializer_group, activity_author: user.activity_object) }
     let(:membership) { create(:socializer_membership, group: group, activity_member: user.activity_object) }
 
-    # Setting the current user
-    before { cookies[:user_id] = user.guid }
-
-    it { should use_before_action(:authenticate_user) }
-
-    describe 'DELETE #destroy' do
-      context 'with valid attributes' do
-        it 'decline the membership' do
-          membership
-          expect { delete :destroy, id: membership.id }.to change(Membership, :count).by(-1)
-        end
-
-        it 'redirects to groups#pending_invites' do
+    describe 'when not logged in' do
+      describe 'DELETE #destroy' do
+        it 'requires login' do
           delete :destroy, id: membership.id
-          expect(response).to redirect_to groups_pending_invites_path
+          expect(response).to redirect_to root_path
         end
       end
+    end
 
-      context 'with invalid attributes' do
-        it 'is a pending example'
+    describe 'when logged in' do
+      # Setting the current user
+      before { cookies[:user_id] = user.guid }
+
+      it { should use_before_action(:authenticate_user) }
+
+      describe 'DELETE #destroy' do
+        context 'with valid attributes' do
+          it 'decline the membership' do
+            membership
+            expect { delete :destroy, id: membership.id }.to change(Membership, :count).by(-1)
+          end
+
+          it 'redirects to groups#pending_invites' do
+            delete :destroy, id: membership.id
+            expect(response).to redirect_to groups_pending_invites_path
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'is a pending example'
+        end
       end
     end
   end
