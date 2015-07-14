3bf3e56afa80ef8ee01a352a4d910d0a8362fa32
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-07-14 14:57:31 -0400

    models/tie: line is too long

--- app/models/socializer/tie.rb
+++ app/models/socializer/tie.rb
@@ -5,16 +5,21 @@ module Socializer
   #
   # Tie model
   #
-  # A {Socializer::Tie} is a link between a {Socializer::Circle} and a {Socializer::Person}
+  # A {Socializer::Tie} is a link between a {Socializer::Circle} and a
+  # {Socializer::Person}
   #
   class Tie < ActiveRecord::Base
     attr_accessible :contact_id
 
     # Relationships
     belongs_to :circle, inverse_of: :ties
-    belongs_to :activity_contact, class_name: "ActivityObject", foreign_key: "contact_id", inverse_of: :ties
+    belongs_to :activity_contact, class_name: "ActivityObject",
+                                  foreign_key: "contact_id",
+                                  inverse_of: :ties
 
-    has_one :contact, through: :activity_contact, source: :activitable,  source_type: "Socializer::Person"
+    has_one :contact, through: :activity_contact,
+                      source: :activitable,
+                      source_type: "Socializer::Person"
 
     # Validations
     validates :circle, presence: true
