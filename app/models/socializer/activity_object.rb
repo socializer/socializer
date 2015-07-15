4319eb530ec2d71259bd4883ea81880b921979a6
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-07-15 11:12:58 -0400

    models/activity_object: line is too long

--- app/models/socializer/activity_object.rb
+++ app/models/socializer/activity_object.rb
@@ -47,9 +47,10 @@ module Socializer
     has_many :ties, foreign_key: "contact_id",
                     inverse_of: :activity_contact
 
-    has_many :memberships, -> { Membership.active },
-                          foreign_key: "member_id",
-                          inverse_of: :activity_member
+    has_many :memberships,
+             -> { Membership.active },
+             foreign_key: "member_id",
+             inverse_of: :activity_member
 
     # Validations
     validates :activitable, presence: true
