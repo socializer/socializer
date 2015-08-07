a63d5e718032ccd901901fa8ff5224db38f25dc6
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-08-07 12:43:28 -0400

    add default address info to the factory

--- spec/factories/socializer_person_addresses.rb
+++ spec/factories/socializer_person_addresses.rb
@@ -4,5 +4,10 @@ FactoryGirl.define do
   factory :person_address, class: Socializer::PersonAddress do
     category :home
     association :person, factory: :person
+    line1 "282 Kevin Brook"
+    city "Imogeneborough"
+    province_or_state "California"
+    postal_code_or_zip "58517"
+    country "US"
   end
 end
