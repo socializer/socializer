5cd0111414de67a89b14af4440364f9714381212
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-08-28 12:36:33 -0400

    label and url are required for person profile

--- db/migrate/20140131070259_create_socializer_person_profiles.rb
+++ db/migrate/20140131070259_create_socializer_person_profiles.rb
@@ -2,8 +2,8 @@ class CreateSocializerPersonProfiles < ActiveRecord::Migration
   def change
     create_table :socializer_person_profiles do |t|
       t.integer  :person_id, null: false
-      t.string   :label
-      t.string   :url
+      t.string   :label, null: false
+      t.string   :url, null: false
 
       t.timestamps null: false
     end
