e46e55bd9b8501a27ba8172eda2a9990f6eec758
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-08-28 12:44:04 -0400

    url is required for person link

--- db/migrate/20140131070232_create_socializer_person_links.rb
+++ db/migrate/20140131070232_create_socializer_person_links.rb
@@ -2,7 +2,7 @@ class CreateSocializerPersonLinks < ActiveRecord::Migration
   def change
     create_table :socializer_person_links do |t|
       t.string   :display_name, null: false
-      t.string   :url
+      t.string   :url, null: false
       t.integer  :person_id, null: false
 
       t.timestamps null: false
