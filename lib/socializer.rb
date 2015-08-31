eb43763e590b0401465f12e2246d690058a4392d
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-08-31 19:45:52 -0400

    put the requires in the same place

--- lib/socializer.rb
+++ lib/socializer.rb
@@ -1,6 +1,22 @@
 require "socializer/engine"
 require "socializer/exceptions"
 
+# Added "jquery-ui-rails" for drag and drop
+require "jquery/ui/rails"
+require "coffee-rails"
+require "sass/rails"
+require "bootstrap-sass"
+require "draper"
+require "enumerize"
+require "omniauth"
+require "omniauth-identity"
+require "omniauth-facebook"
+require "omniauth-linkedin"
+require "omniauth-openid"
+require "omniauth-twitter"
+require "simple_form"
+require "country_select"
+
 #
 # Namespace for the Socializer engine
 #
