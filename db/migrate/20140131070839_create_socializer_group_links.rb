ac0819a4db769b97485b3a6208f46fef4edbd255
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-08-31 13:48:44 -0400

    display_name and url are required for a group link
    /cc #69

--- db/migrate/20140131070839_create_socializer_group_links.rb
+++ db/migrate/20140131070839_create_socializer_group_links.rb
@@ -2,8 +2,8 @@ class CreateSocializerGroupLinks < ActiveRecord::Migration
   def change
     create_table :socializer_group_links do |t|
       t.integer  :group_id, null: false
-      t.string   :display_name
-      t.string   :url
+      t.string   :display_name, null: false
+      t.string   :url, null: false
 
       t.timestamps null: false
     end
