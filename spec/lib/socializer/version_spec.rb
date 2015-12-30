86b1922af56136ad98df1588c466746b1822a8d8
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-12-30 12:34:18 -0500

    add a version spec

---
+++ spec/lib/socializer/version_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+module Socializer
+  RSpec.describe VERSION do
+    it { is_expected.to be_a String }
+    it { is_expected.to match_regex(/\d+.\d+.\d+(-[a-zA-Z0-9]+)*/) }
+    it { is_expected.to_not be_nil }
+  end
+end
