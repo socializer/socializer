441d8803c4411c0cc7adc984373cab0fa788317e
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-03-02 11:02:25 -0500

    add database constraints to require some fields

--- db/migrate/20110805094846_create_socializer_comments.rb
+++ db/migrate/20110805094846_create_socializer_comments.rb
@@ -1,8 +1,8 @@
 class CreateSocializerComments < ActiveRecord::Migration
   def change
     create_table :socializer_comments do |t|
-      t.integer  :author_id
-      t.text     :content
+      t.integer  :author_id, null: false
+      t.text     :content,   null: false
 
       t.timestamps null: false
     end
