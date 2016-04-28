dac75bed2abd5e133e97c36abfefb0c878d52672
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-04-27 20:31:37 -0400

    remove old code

--- app/controllers/socializer/activities/shares_controller.rb
+++ app/controllers/socializer/activities/shares_controller.rb
@@ -19,9 +19,6 @@ module Socializer
       def create
         share = params[:share]
         activity_object = find_activity_object(id: share[:activity_id])
-        # activity_object.share(actor_id: current_user.guid,
-        #                       object_ids: share[:object_ids].split(","),
-        #                       content: share[:content])
 
         ActivityObject::Services::Share
           .new(actor: current_user,
