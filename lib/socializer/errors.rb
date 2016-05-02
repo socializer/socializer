301913bd47b3a86a8a9a468a4983b871f391a32a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-05-02 11:21:15 -0400

    add some documentation for module Errors
    [skip ci]

--- lib/socializer/errors.rb
+++ lib/socializer/errors.rb
@@ -2,22 +2,28 @@
 # Namespace for the Socializer engine
 #
 module Socializer
+  #
   # Base error class
+  #
   class Error < StandardError
   end
 
+  #
+  # Module Errors provides engine specific errors
+  #
   module Errors
     # Raised when the record is invalid.
     class RecordInvalid < Socializer::Error
     end
 
+    #
     # Raised when someone tries to join a private goup instead of being invited.
+    #
     class PrivateGroupCannotSelfJoin < Socializer::Error
       #
       # Override Exception#message
       #
       # @return [String] Returns the error's message
-      #
       def message
         I18n.t("socializer.errors.messages.group.private.cannot_self_join")
       end
