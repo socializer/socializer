aa2660336ac7fe79fd0111a8ca39b728890fc359
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-07-13 11:32:28 -0400

    Style/ConditionalAssignment: Use the return of the conditional for variable assignment and comparison.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -45,14 +45,16 @@ RSpec.configure do |config|
     # with the specs, so we can use transaction strategy for speed.
     shares_db_connection_with_specs = Capybara.current_driver == :rack_test
 
-    if shares_db_connection_with_specs
-      DatabaseCleaner.strategy = :transaction
-    else
-      # Non-:rack_test driver is probably a driver for a JavaScript browser
-      # with a Rack app under test that does *not* share a database
-      # connection with the specs, so we must use truncation strategy.
-      DatabaseCleaner.strategy = :truncation
-    end
+    DatabaseCleaner.strategy = if shares_db_connection_with_specs
+                                 :transaction
+                               else
+                                 # Non-:rack_test driver is probably a driver
+                                 # for a JavaScript browser with a Rack app
+                                 # under test that does *not* share a database
+                                 # connection with the specs, so we must use
+                                 # truncation strategy.
+                                 :truncation
+                               end
   end
 
   config.before(:each) do
