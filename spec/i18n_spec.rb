51033e677d1790007adaa0eb293df6ab6f8c7a62
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-07-13 13:07:36 -0400

    RSpec/DescribeClass: The first argument to describe should be the class or module being tested.

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require "i18n/tasks"
 
-RSpec.describe "I18n" do
+RSpec.describe I18n do
   let(:i18n) { I18n::Tasks::BaseTask.new }
   let(:missing_keys) { i18n.missing_keys }
   let(:unused_keys) { i18n.unused_keys }
