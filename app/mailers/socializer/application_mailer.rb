f327dbcc1cf3b7ede4ee6f18abb2d8459f7e557b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-08-24 14:54:57 -0400

    Style/Documentation: Missing top-level class documentation comment.

--- app/mailers/socializer/application_mailer.rb
+++ app/mailers/socializer/application_mailer.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+#
+# Namespace for the Socializer engine
+#
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
--- app/models/socializer/application_record.rb
+++ app/models/socializer/application_record.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+#
+# Namespace for the Socializer engine
+#
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
