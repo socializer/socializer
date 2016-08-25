6037651a7346f318479bcc5ebd8c159ae87c0c83
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-08-25 08:44:08 -0400

    add specs for application_job and application_mailer

---
+++ spec/jobs/socializer/application_job_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+module Socializer
+  RSpec.describe ApplicationJob, type: :job do
+    include ActiveJob::TestHelper
+
+    it "is a pending example"
+  end
+end
---
+++ spec/mailers/socializer/application_mailer_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Socializer
+  RSpec.describe ApplicationMailer, type: :mailer do
+    it "is a pending example"
+  end
+end
