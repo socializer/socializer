5e121ce1d7c75278f213710b62783ffdc898f549
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2017-05-05 09:54:04 -0400

    add back protect_from_forgery

--- app/controllers/socializer/application_controller.rb
+++ app/controllers/socializer/application_controller.rb
@@ -8,6 +8,8 @@ module Socializer
   # Application controller
   #
   class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
     helper_method :current_user
     helper_method :signed_in?
 
