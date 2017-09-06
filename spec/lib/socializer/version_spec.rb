bf1032300054d9e6c3e893123992efe4a6ac3989
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2017-09-06 17:48:17 -0400

    RSpec/LetBeforeExamples: Move let before the examples in the group.

--- spec/lib/socializer/version_spec.rb
+++ spec/lib/socializer/version_spec.rb
@@ -4,15 +4,14 @@ require "rails_helper"
 
 module Socializer
   RSpec.describe VERSION do
+    let(:gemspec_path) { Engine.root.join("socializer.gemspec").to_s }
+    let(:specification) { Gem::Specification.load(gemspec_path) }
+
     it { is_expected.to be_a String }
     it { is_expected.to match_regex(/\d+.\d+.\d+(-[a-zA-Z0-9]+)*/) }
     it { is_expected.not_to be_nil }
 
     it { expect(Socializer::VERSION.frozen?).to be true }
-
-    let(:gemspec_path) { Engine.root.join("socializer.gemspec").to_s }
-    let(:specification) { Gem::Specification.load(gemspec_path) }
-
     it { expect(Socializer::VERSION).to eq(specification.version.to_s) }
   end
 end
