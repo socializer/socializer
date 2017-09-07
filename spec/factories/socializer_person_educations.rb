586c6d30bbe3cf2ad3f71126f18411321e8cd92b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2017-09-06 20:30:28 -0400

    FactoryGirl/DynamicAttributeDefinedStatically: Use a block to set a dynamic value to an attribute.

--- spec/factories/socializer_person_educations.rb
+++ spec/factories/socializer_person_educations.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
   factory :person_education, class: Socializer::Person::Education do
     school_name "Hard Knocks"
     major_or_field_of_study "Slacking"
-    started_on Date.new(2012, 12, 3)
+    started_on { Date.new(2012, 12, 3) }
     ended_on nil
     current true
     association :person, factory: :person
