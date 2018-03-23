97a888424a2112481cd4c1ccd22c8f8514d1b4f7
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2018-03-23 09:04:27 -0400

    Style/RedundantBegin: Redundant begin block detected.

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -6,12 +6,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-      # Test factories in spec/factories are working.
-      FactoryBot.lint
-    ensure
-      DatabaseCleaner.clean
-    end
+    DatabaseCleaner.start
+    # Test factories in spec/factories are working.
+    FactoryBot.lint
+  ensure
+    DatabaseCleaner.clean
   end
 end
