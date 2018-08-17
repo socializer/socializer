c52869c7e0d298a1392d2b44d5de6b1e7f7788f5
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2018-08-17 10:51:10 -0400

    change ArgumentError to Dry::Types::ConstraintError

--- spec/services/socializer/activity_audience_list_spec.rb
+++ spec/services/socializer/activity_audience_list_spec.rb
@@ -8,20 +8,20 @@ module Socializer
       describe ".new should raise an ArgumentError" do
         let(:audience_list) { ActivityAudienceList.new(activity: nil) }
 
-        it { expect { audience_list }.to raise_error(ArgumentError) }
+        it { expect { audience_list }.to raise_error(Dry::Types::ConstraintError) }
       end
 
       describe ".call should raise an ArgumentError" do
         let(:audience_list) { ActivityAudienceList.call(activity: nil) }
 
-        it { expect { audience_list }.to raise_error(ArgumentError) }
+        it { expect { audience_list }.to raise_error(Dry::Types::ConstraintError) }
       end
     end
 
     describe "when the activity argument is the wrong type" do
       let(:audience_list) { ActivityAudienceList.new(activity: Person.new) }
 
-      it { expect { audience_list }.to raise_error(ArgumentError) }
+      it { expect { audience_list }.to raise_error(Dry::Types::ConstraintError) }
     end
 
     describe ".call" do
