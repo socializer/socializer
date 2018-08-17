b08edc172ed048e32317715eea6375ebf4cd82fd
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2018-08-17 11:17:18 -0400

    Metrics/LineLength: Line is too long.

--- spec/services/socializer/activity_audience_list_spec.rb
+++ spec/services/socializer/activity_audience_list_spec.rb
@@ -8,20 +8,26 @@ module Socializer
       describe ".new should raise an ArgumentError" do
         let(:audience_list) { ActivityAudienceList.new(activity: nil) }
 
-        it { expect { audience_list }.to raise_error(Dry::Types::ConstraintError) }
+        it do
+          expect { audience_list }.to raise_error(Dry::Types::ConstraintError)
+        end
       end
 
       describe ".call should raise an ArgumentError" do
         let(:audience_list) { ActivityAudienceList.call(activity: nil) }
 
-        it { expect { audience_list }.to raise_error(Dry::Types::ConstraintError) }
+        it do
+          expect { audience_list }.to raise_error(Dry::Types::ConstraintError)
+        end
       end
     end
 
     describe "when the activity argument is the wrong type" do
       let(:audience_list) { ActivityAudienceList.new(activity: Person.new) }
 
-      it { expect { audience_list }.to raise_error(Dry::Types::ConstraintError) }
+      it do
+        expect { audience_list }.to raise_error(Dry::Types::ConstraintError)
+      end
     end
 
     describe ".call" do
