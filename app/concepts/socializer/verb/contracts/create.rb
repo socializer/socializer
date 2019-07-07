1bd2fcbcd80a3effd600738b31723d6dfb2f7dcc
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2019-07-07 11:42:57 -0400

    add a Verb contract

---
+++ app/concepts/socializer/verb/contracts/create.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "dry-validation"
+
+#
+# Namespace for the Socializer engine
+#
+module Socializer
+  #
+  # Namespace for Verb related objects
+  #
+  class Verb
+    #
+    # Namespace for Contract related objects
+    #
+    module Contracts
+      #
+      # Contract object for vaidating Socializer::Verb
+      #
+      # @example
+      #   contract = Verb::Contracts::Create.new
+      #   result = contract.call(params)
+      class Create < Dry::Validation::Contract
+        params do
+          required(:display_name).filled(Types::ActivityVerbs)
+        end
+      end
+    end
+  end
+end
