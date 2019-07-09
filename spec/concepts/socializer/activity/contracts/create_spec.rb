8ffa6625343eb8a3ef3f41f655301539f71a600e
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2019-07-09 16:36:11 -0400

    create tests for the contracts

---
+++ spec/concepts/socializer/activity/contracts/create_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Socializer
+  class Activity
+    module Contracts
+      RSpec.describe Create, type: :contract do
+        let(:result) { subject.call(attributes) }
+        let(:verb) { build(:verb) }
+
+        context "when attributes are specified" do
+          let(:attributes) do
+            { actor_id: 1,
+              activity_object_id: 1,
+              verb: verb,
+              object_ids: "public",
+              content: "share" }
+          end
+
+          it "is valid" do
+            expect(result).to be_success
+          end
+        end
+
+        context "when attributes are not specified" do
+          let(:attributes) { { } }
+
+          it { expect(result).to be_failure }
+          it { expect(result.errors[:actor_id]).to eq(["is missing"]) }
+
+          it do
+            expect(result.errors[:activity_object_id]).to eq(["is missing"])
+          end
+
+          it { expect(result.errors[:verb]).to eq(["is missing"]) }
+          it { expect(result.errors[:object_ids]).to be_nil }
+          it { expect(result.errors[:content]).to be_nil }
+        end
+      end
+    end
+  end
+end
---
+++ spec/concepts/socializer/verb/contracts/create_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Socializer
+  class Verb
+    module Contracts
+      RSpec.describe Create, type: :contract do
+        let(:result) { subject.call(attributes) }
+
+        context "when attributes are specified" do
+          let(:attributes) do
+            { display_name: "unlike" }
+          end
+
+          it "is valid" do
+            expect(result).to be_success
+          end
+        end
+
+        context "when attributes are not specified" do
+          let(:attributes) { { } }
+
+          it { expect(result).to be_failure }
+          it { expect(result.errors[:display_name]).to eq(["is missing"]) }
+        end
+
+        context "when attributes are invalid" do
+          let(:attributes) do
+            { display_name: "bob" }
+          end
+
+          let(:valid_verbs) { Types::ActivityVerbs }
+
+          it { expect(result).to be_failure }
+
+          it do
+            expect(result.errors[:display_name])
+              .to eq(["must be one of: #{valid_verbs.values.join(', ')}"])
+          end
+        end
+      end
+    end
+  end
+end
