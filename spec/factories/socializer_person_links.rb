4c59668506e6741a310a8a0f79a88c9888b1f3da
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2019-11-27 12:22:06 -0500

    FactoryBot/FactoryClassName: Pass 'Socializer::Person::Link' instead of Socializer::Person::Link.

--- spec/factories/socializer_person_links.rb
+++ spec/factories/socializer_person_links.rb
@@ -3,7 +3,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_bot
 
 FactoryBot.define do
-  factory :person_link, class: Socializer::Person::Link do
+  factory :person_link, class: "Socializer::Person::Link" do
     display_name { "test" }
     url { "http://test.org" }
     association :person, factory: :person
