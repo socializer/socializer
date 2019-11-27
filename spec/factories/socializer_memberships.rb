c6f29f227292827a7431411f0ab40af5c9e1c638
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2019-11-27 12:32:07 -0500

    FactoryBot/FactoryClassName: Pass 'Socializer::Membership' instead of Socializer::Membership.

--- spec/factories/socializer_memberships.rb
+++ spec/factories/socializer_memberships.rb
@@ -3,7 +3,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_bot
 
 FactoryBot.define do
-  factory :socializer_membership, class: Socializer::Membership do
+  factory :socializer_membership, class: "Socializer::Membership" do
     active { true }
     association :group, factory: :group, strategy: :create
     association :activity_member, factory: :activity_object_person
