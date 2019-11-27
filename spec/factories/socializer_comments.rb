8ddd4cc38dd01bd6c66f44e8aa7773f6bbe04d2b
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2019-11-27 12:35:57 -0500

    FactoryBot/FactoryClassName: Pass 'Socializer::Comment' instead of Socializer::Comment.

--- spec/factories/socializer_comments.rb
+++ spec/factories/socializer_comments.rb
@@ -3,7 +3,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_bot
 
 FactoryBot.define do
-  factory :comment, class: Socializer::Comment do
+  factory :comment, class: "Socializer::Comment" do
     content { "This is a comment" }
     association :activity_author, factory: :activity_object_person
   end
