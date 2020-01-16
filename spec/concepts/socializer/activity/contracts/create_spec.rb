4385c7761c826eb5d5a5b4459ed459d94c7e4201
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-01-16 14:43:07 -0500

    test when object_ids are an integer and array of integers

--- spec/concepts/socializer/activity/contracts/create_spec.rb
+++ spec/concepts/socializer/activity/contracts/create_spec.rb
@@ -7,15 +7,21 @@ module Socializer
     let(:result) { subject.call(attributes) }
     let(:verb) { build(:verb) }
 
-    context "when attributes are specified" do
-      let(:attributes) do
-        { actor_id: 1,
-          activity_object_id: 1,
-          verb: verb,
-          object_ids: "public",
-          content: "share" }
-      end
+    let(:public_privacy) do
+      Socializer::Audience.privacy.find_value(:public).value
+    end
 
+    let(:object_ids) { public_privacy }
+
+    let(:attributes) do
+      { actor_id: 1,
+        activity_object_id: 1,
+        verb: verb,
+        object_ids: object_ids,
+        content: "share" }
+    end
+
+    context "when attributes are specified" do
       it "is valid" do
         expect(result).to be_success
       end
@@ -34,6 +40,38 @@ module Socializer
       it { expect(result.errors[:verb]).to eq(["is missing"]) }
       it { expect(result.errors[:object_ids]).to be_nil }
       it { expect(result.errors[:content]).to be_nil }
+    end
+
+    context "when #object_ids is an Integer" do
+      describe "and the circle don't exist" do
+        let(:object_ids) { 1 }
+
+        it { expect(result).to be_failure }
+      end
+
+      describe "and the circle exists" do
+        let(:circle) { create(:circle) }
+        let(:object_ids) { circle.id }
+
+        it { expect(result).to be_success }
+      end
+    end
+
+    context "when #object_ids is an array of Integers" do
+      describe "and the circles doesn't exist" do
+        let(:object_ids) { [1, 2, 3] }
+
+        it { expect(result).to be_failure }
+      end
+
+      describe "and the circles do exists" do
+        let(:circle1) { create(:circle) }
+        let(:circle2) { create(:circle) }
+        let(:circle3) { create(:circle) }
+        let(:object_ids) { [circle1.id, circle2.id, circle3.id] }
+
+        it { expect(result).to be_success }
+      end
     end
   end
 end
