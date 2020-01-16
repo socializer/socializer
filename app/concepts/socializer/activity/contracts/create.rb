ee16941e8f924e849c6efad97038da02e5bd291f
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-01-16 15:06:41 -0500

    initial rules for checking teh Circle exists when object_ids are integers or an array of integers

--- app/concepts/socializer/activity/contracts/create.rb
+++ app/concepts/socializer/activity/contracts/create.rb
@@ -31,6 +31,7 @@ module Socializer
           # FIXME: The object_ids is causing an issue. should allow integers
           #        too. The Integers should be a valid Circle. Check in a rule.
           #        The str? should also be checked against PRIVACY
+          # optional(:object_ids)
           optional(:object_ids).maybe do
             included_in?(PRIVACY) | int? |
               array? & each { included_in?(PRIVACY) | int? }
@@ -39,6 +40,47 @@ module Socializer
             # end
           end
           optional(:content).maybe(:string)
+        end
+
+        rule(:object_ids) do
+          # byebug
+          if value.is_a?(Integer)
+            check_circle(id: value, key: key)
+            # circle = Circle.find_by(id: value)
+            # key.failure("could not be found") if circle.blank?
+          end
+        end
+
+        rule(:object_ids) do
+          if value.is_a?(Array)
+            value.each do |item|
+              next unless item.is_a?(Integer)
+
+              check_circle(id: item, key: key)
+              # circle = Circle.find_by(id: item)
+              # key.failure("could not be found") if circle.blank?
+            end
+          end
+        end
+
+        # rule(:object_ids) do
+        #   # byebug
+        #   if value.is_a?(String)
+        #     key.failure("could not be found") if PRIVACY.exclude?(value)
+        #   end
+        # end
+
+        # rule(:object_ids) do
+        #   # byebug
+        #   if value.is_a?(Array)
+        #     key.failure("could not be found") if (PRIVACY & value).blank?
+        #   end
+        # end
+        private
+
+        def check_circle(id:, key:)
+          circle = Circle.find_by(id: id)
+          key.failure("could not be found") if circle.blank?
         end
       end
     end
