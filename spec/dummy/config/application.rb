1917d0960e324e64138e4b199ee18dc32e0171b5
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-04-11 15:29:32 -0400

    run rails app:app:update to update the dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,17 +1,10 @@
 require_relative 'boot'
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
-require "sprockets/railtie"
+require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "socializer"
 
 module Dummy
   class Application < Rails::Application
@@ -19,9 +12,8 @@ module Dummy
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    config.active_record.sqlite3.represent_boolean_as_integer = true
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
