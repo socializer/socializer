e3e1e7c26cf96b1ac7860b10ca3b858d5cddf7aa
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-04-12 00:09:26 -0400

    Style/IfUnlessModifier: Modifier form of if makes the line too long.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,8 +11,12 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("dummy/config/environment", __dir__)
+
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+if Rails.env.production?
+  abort("The Rails environment is running in production mode!")
+end
+
 require "rspec/rails"
 
 # Add additional requires below this line. Rails is not loaded until this point!
