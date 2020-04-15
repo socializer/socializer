2b6ba5ec62a90bb6ddaf3b21d078f0ad69223248
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-04-15 14:11:36 -0400

    get omniauth working again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,6 +10,7 @@ require "omniauth-openid"
 require "openid/store/filesystem"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :open_id, store: OpenID::Store::Filesystem.new("/tmp"),
                      name: "yahoo",
                      identifier: "yahoo.com"
