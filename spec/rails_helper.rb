5df2e9a81f4645aaabe9a46d299411a80687582a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-04-24 17:38:05 -0400

    update spec_helper and rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This file is copied to spec/ when you run "rails generate rspec:install"
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 require "simplecov"
 require "coveralls"
 
@@ -8,42 +8,59 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
 )
 
-# This file is copied to spec/ when you run "rails generate rspec:install"
+require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("dummy/config/environment", __dir__)
+
+# Prevent database truncation if the environment is production
+if Rails.env.production?
+  abort("The Rails environment is running in production mode!")
+end
+
 require "rspec/rails"
 
-ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), "../")
+# Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
 # in _spec.rb will both be required and run as specs, causing the specs to be
 # run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with with the --pattern
+# end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].sort.each do |file|
-  require file
+#
+# The following line is provided for convenience purposes. It has the downside
+# of increasing the boot-up time by auto-requiring all files in the support
+# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# require only the support files necessary.
+#
+# root = Rails.root
+# Dir[root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
+root = Socializer::Engine.root
+Dir[root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
 end
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
-
 RSpec.configure do |config|
   # Eliminate the need to use I18n.t(). With this we can use t()
   config.include ActionView::Helpers::TranslationHelper
 
-  # Remove this line if you"re not using ActiveRecord or ActiveRecord fixtures
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # See database_cleaner.rb about config.use_transactional_fixtures
-  #
-  # If you"re not using ActiveRecord, or you"d prefer not to run each of your
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
-  # instead of true.  #
+  # instead of true.
   # config.use_transactional_fixtures = true
+
+  # You can uncomment this line to turn off ActiveRecord support entirely.
+  # config.use_active_record = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -52,11 +69,16 @@ RSpec.configure do |config|
   # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
-  #     RSpec.describe UsersController, :type => :controller do
+  #     RSpec.describe UsersController, type: :controller do
   #       # ...
   #     end
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
 end
