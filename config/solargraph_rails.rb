122c16880f4d32fe0389250553f12a43583aafb0
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-05-21 10:12:35 -0400

    Style/FrozenStringLiteralComment: Missing frozen string literal comment.

--- config/solargraph_rails.rb
+++ config/solargraph_rails.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # The following comments fill some of the gaps in Solargraph's understanding of
 # Rails apps. Since they're all in YARD, they get mapped in Solargraph but
 # ignored at runtime.
