148e67d9f9c1c81d45834d8d93ab34467ce1a3dc
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-08-27 13:39:40 -0400

    add a note for ActivityVerbs

--- config/initializers/dry_types.rb
+++ config/initializers/dry_types.rb
@@ -11,5 +11,16 @@ module Types
   StrippedString = Types::String.constructor(&:strip)
   LowercaseString = Types::String.constructor(&:downcase)
 
+  # NOTE: Check Verbs against ActivityStreams 2.0.
+  #       post -> create or add depending on the context
+  #       share -> announce with audience targeting
+  #       unlike -> undo where object is the like
+  #       {
+  #         "type": "Undo",
+  #         "object": {
+  #           "type": "Like",
+  #           "object": "http://foo.example/obj/blarg/"
+  #       }
+  #     }
   ActivityVerbs = Types::String.enum("add", "like", "post", "share", "unlike")
 end
