8631cb3689af36cd4aef5aeb946c17fb96fe9088
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2022-04-21 13:16:55 -0400

    provide default values for ENV.fetch

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,7 +10,7 @@ require "omniauth-openid"
 require "openid/store/filesystem"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
+  provider :developer if Rails.env.development?
   provider :open_id, store: OpenID::Store::Filesystem.new("/tmp"),
                      name: "yahoo",
                      identifier: "yahoo.com"
@@ -19,9 +19,17 @@ Rails.application.config.middleware.use OmniAuth::Builder do
                      name: "google",
                      identifier: "https://www.google.com/accounts/o8/id"
 
-  provider :twitter,  ENV.fetch("TWITTER_KEY"), ENV.fetch("TWITTER_SECRET")
-  provider :facebook, ENV.fetch("FACEBOOK_KEY"), ENV.fetch("FACEBOOK_SECRET")
-  provider :linkedin, ENV.fetch("LINKEDIN_KEY"), ENV.fetch("LINKEDIN_SECRET")
+  provider :twitter,
+           ENV.fetch("TWITTER_KEY", nil),
+           ENV.fetch("TWITTER_SECRET", nil)
+
+  provider :facebook,
+           ENV.fetch("FACEBOOK_KEY", nil),
+           ENV.fetch("FACEBOOK_SECRET", nil)
+
+  provider :linkedin,
+           ENV.fetch("LINKEDIN_KEY", nil),
+           ENV.fetch("LINKEDIN_SECRET", nil)
 
   provider :identity, model: Socializer::Identity
 end
