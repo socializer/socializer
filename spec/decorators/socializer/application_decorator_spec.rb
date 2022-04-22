b4499183d23d3e4af8bf05afb5801f253133a757
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2022-04-22 15:23:11 -0400

    RSpec/MultipleMemoizedHelpers: Example group has too many memoized helpers

--- spec/decorators/socializer/application_decorator_spec.rb
+++ spec/decorators/socializer/application_decorator_spec.rb
@@ -5,31 +5,30 @@ require "rails_helper"
 module Socializer
   RSpec.describe ApplicationDecorator, type: :decorator do
     describe "created_at_time_ago" do
-      let(:activity) { create(:activity) }
-      let(:decorated_activity) { ActivityDecorator.new(activity) }
-      let(:created_at) { activity.created_at.utc }
-      let(:updated_at) { activity.updated_at.utc }
+      let(:decorated_activity) { ActivityDecorator.new(create(:activity)) }
+      let(:created_at) { decorated_activity.created_at.utc }
+      let(:updated_at) { decorated_activity.updated_at.utc }
+      let(:time_text) { created_at.strftime("%B %e, %Y %l:%M%P") }
 
       context "when created_at and updated_at are equal" do
-        let(:result) { decorated_activity.created_at_time_ago }
-        let(:time_text) { created_at.strftime("%B %e, %Y %l:%M%P") }
-
         specify do
-          expect(result).to have_selector("time", text: time_text)
+          expect(decorated_activity.created_at_time_ago)
+            .to have_selector("time", text: time_text)
         end
 
         specify do
-          expect(result)
+          expect(decorated_activity.created_at_time_ago)
             .to have_selector("time[datetime='#{created_at.iso8601}']")
         end
 
         specify do
-          expect(result)
+          expect(decorated_activity.created_at_time_ago)
             .to have_selector("time[title='#{created_at.to_s(:short)}']")
         end
 
         specify do
-          expect(result).to have_selector("time[data-time-ago='moment.js']")
+          expect(decorated_activity.created_at_time_ago)
+            .to have_selector("time[data-time-ago='moment.js']")
         end
       end
 
@@ -39,29 +38,28 @@ module Socializer
           decorated_activity.save!
         end
 
-        let(:result) { decorated_activity.created_at_time_ago }
-        let(:time_text) { created_at.strftime("%B %e, %Y %l:%M%P") }
-
         let(:time_title) do
           "#{created_at.to_s(:short)} (edited #{updated_at.to_s(:short)})"
         end
 
         specify do
-          expect(result).to have_selector("time", text: time_text)
+          expect(decorated_activity.created_at_time_ago)
+            .to have_selector("time", text: time_text)
         end
 
         specify do
-          expect(result)
+          expect(decorated_activity.created_at_time_ago)
             .to have_selector("time[datetime='#{created_at.iso8601}']")
         end
 
         specify do
-          expect(result)
+          expect(decorated_activity.created_at_time_ago)
             .to have_selector("time[title='#{time_title}']")
         end
 
         specify do
-          expect(result).to have_selector("time[data-time-ago='moment.js']")
+          expect(decorated_activity.created_at_time_ago)
+            .to have_selector("time[data-time-ago='moment.js']")
         end
       end
     end
