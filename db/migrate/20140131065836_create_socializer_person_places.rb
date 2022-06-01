a23b4d1346b8e6598d517730e0f6118a77db9d53
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2022-06-01 17:58:53 -0400

    add `NOT NULL` to socializer_person_places.city_name - models validates its presence but it's not non-NULL in the database

--- db/migrate/20140131065836_create_socializer_person_places.rb
+++ db/migrate/20140131065836_create_socializer_person_places.rb
@@ -4,7 +4,7 @@ class CreateSocializerPersonPlaces < ActiveRecord::Migration[7.0]
   def change
     create_table :socializer_person_places do |t|
       t.references :person, null: false
-      t.string   :city_name
+      t.string   :city_name, null: false
       t.boolean  :current, default: false
 
       t.timestamps
