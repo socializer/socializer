af8eb08b4fbeb6e726d7c9321b7cdb5f4b3d65fd
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2022-11-24 19:45:53 -0500

    Rails/FilePath: Prefer Rails.root.join('path/to').

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,7 @@ RSpec.configure do |config|
   config.include ActionView::Helpers::TranslationHelper
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join("spec/fixtures")
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
