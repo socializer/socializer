3777e19c47d91d1800b26a307b1ba3172f953b00
acmetech <info@acmetechnologygroup.com>
2024-01-06 14:33:54 -0500

    brakeman Weak Hash

--- app/decorators/socializer/person_decorator.rb
+++ app/decorators/socializer/person_decorator.rb
@@ -189,7 +189,7 @@ module Socializer
     def gravatar_url
       return if email.blank?
 
-      hash = Digest::MD5.hexdigest(email.downcase.strip)
+      hash = Digest::SHA256.hexdigest(email.downcase.strip)
 
       "https://www.gravatar.com/avatar/#{hash}"
     end
