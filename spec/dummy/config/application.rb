aafb4643c65fef02519184b757ed7845ec4d7b7b
acmetech <info@acmetechnologygroup.com>
2024-01-11 22:28:23 -0500

    change the order of the run commands for "prepare the database"

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -17,7 +17,6 @@ require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "socializer"
 
 module Dummy
   class Application < Rails::Application
