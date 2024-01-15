d570f7a3e947d797b955a782b93001f0e094b977
acmetech <info@acmetechnologygroup.com>
2024-01-15 12:10:28 -0500

    Rails/FilePath: Prefer Rails.root.join('path', 'to').

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,7 @@ RSpec.configure do |config|
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
-    Rails.root.join("spec/fixtures")
+    Rails.root.join("spec", "fixtures")
   ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
