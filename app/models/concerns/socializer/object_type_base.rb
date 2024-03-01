b914eed80e8eb0acfdaf0fab4e340f0cd19cc61b
acmetech <info@acmetechnologygroup.com>
2024-03-01 08:58:18 -0500

    update the joins statement

--- app/models/concerns/socializer/object_type_base.rb
+++ app/models/concerns/socializer/object_type_base.rb
@@ -30,10 +30,7 @@ module Socializer
       #
       # @return [ActiveRecord::Relation]
       def guids
-        # FIXME: Rails 7.1 - https://github.com/rails/rails/pull/45612 -
-        #        Allows using relation name when querying joins/includes
-        # joins(:activity_object).select(activity_object: [id:])
-        joins(:activity_object).select("socializer_activity_objects.id")
+        joins(:activity_object).select(socializer_activity_objects: [:id])
       end
     end
 
