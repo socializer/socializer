9242a20abb05f63dae1f4494891bf0a5ba5e4639
acmetech <info@acmetechnologygroup.com>
2024-03-29 15:09:23 -0400

    add index as part of the attribute definition

--- db/migrate/20110804192211_create_socializer_authentications.rb
+++ db/migrate/20110804192211_create_socializer_authentications.rb
@@ -3,15 +3,12 @@
 class CreateSocializerAuthentications < ActiveRecord::Migration[7.1]
   def change
     create_table :socializer_authentications do |t|
-      t.bigint :person_id, null: false
-      t.string :provider, null: false
+      t.bigint :person_id, null: false, index: true
+      t.string :provider, null: false, index: true
       t.string :uid, null: false
       t.string :image_url
 
       t.timestamps
     end
-
-    add_index :socializer_authentications, :person_id
-    add_index :socializer_authentications, :provider
   end
 end
