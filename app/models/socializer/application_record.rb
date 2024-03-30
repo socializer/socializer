97af7b8468679b86d130e6b251bcdc20053c4402
acmetech <info@acmetechnologygroup.com>
2024-03-30 00:21:39 -0400

    Style/Documentation: Missing top-level documentation comment

--- app/models/socializer/application_record.rb
+++ app/models/socializer/application_record.rb
@@ -4,7 +4,16 @@
 # Namespace for the Socializer engine
 #
 module Socializer
-  # Base class for ActiveRecord models
+  # The ApplicationRecord class is an abstract base class that
+  # other models in the application inherit from.
+  # It is a direct subclass of ActiveRecord::Base.
+  #
+  # This class is primarily used for defining methods and behaviors
+  # that are shared across all models in the application.
+  #
+  # The `primary_abstract_class` method call indicates that this class
+  # is abstract, and therefore, instances of ApplicationRecord should not be
+  # created. This method is provided by ActiveRecord.
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
   end
