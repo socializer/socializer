e94255d8146244165e351467392cc164f56b017d
acmetech <info@acmetechnologygroup.com>
2025-02-10 12:22:32 -0500

    be specific

--- app/models/socializer/activity.rb
+++ app/models/socializer/activity.rb
@@ -263,7 +263,7 @@ module Socializer
 
       query.where(Audience.public_privacy_grouping(viewer_id:)
            .or(Audience.limited_privacy_grouping(viewer_id:))
-           .or(where(actor_id: viewer_id)))
+           .or(Audience.where(actor_id: viewer_id)))
     end
     private_class_method :stream_query
 
