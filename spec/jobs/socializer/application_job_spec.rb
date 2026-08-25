cf566a66123509151fc3938523ddf425af8b3146
acmetech <info@acmetechnologygroup.com>
2026-08-25 14:25:29 -0400

    Style/DirectiveScope: Use disable-next instead of a disable/enable pair around a single statement.

--- spec/jobs/socializer/application_job_spec.rb
+++ spec/jobs/socializer/application_job_spec.rb
@@ -3,9 +3,8 @@
 require "rails_helper"
 
 module Socializer
-  # rubocop:disable RSpec/EmptyExampleGroup
+  # rubocop:disable-next RSpec/EmptyExampleGroup
   RSpec.describe ApplicationJob do
     include ActiveJob::TestHelper
   end
-  # rubocop:enable RSpec/EmptyExampleGroup
 end
